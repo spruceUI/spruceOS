2934a2d75c4b3d7de98c0f4c26f9028556f57d4d
spruceOS <177474238+spruceUI@users.noreply.github.com>
2024-09-08 12:53:17 -0700

    Add files via upload

--- App/aleatorio/aleatorio.sh
+++ App/aleatorio/aleatorio.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
-# Cambia temporalmente al directorio y obtén la ruta completa
+
 cd "$(dirname "$1")" || exit
 SIMPLIFIED_PATH=$(pwd)
-# Extraer el último directorio de la ruta
 CARPETA=$(basename "$SIMPLIFIED_PATH")
-# Ruta base donde están los directorios de ROMs
 BASE_DIR="/mnt/SDCARD/Roms"
 EMU_DIR="/mnt/SDCARD/Emu"
 CONFIG_PATH=$EMU_DIR/$CARPETA
 CABEZA_ESTADO=$(head -n 14 /tmp/state.json)
 PLATAFORMA=$(grep '"label":' $CONFIG_PATH/config.json | sed 's/.*"label":\s*"\([^"]*\)".*/\1/')
-# Función para obtener un número aleatorio basado en la cantidad de archivos válidos
+
+LAST_SELECTED_FILE="/tmp/last_selected_game.txt"
+
 get_random_number() {
     dir="/mnt/SDCARD/Roms/$CARPETA"
-    # Contar solo archivos en el directorio principal sin subdirectorios
     file_count=$(find "$dir" -maxdepth 1 -type f ! -name "*.db" ! -name "*.xml" ! -name "*.png" | wc -l)
     if [ "$file_count" -gt 0 ]; then
         random_number=$(awk -v min=0 -v max="$file_count" 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
@@ -24,16 +23,19 @@ get_random_number() {
     fi
 }
 
-selected_dir="$CARPETA"
-# Verificar si se ha encontrado un directorio válido
-if [ -n "$selected_dir" ]; then
-    echo "Directorio seleccionado: $selected_dir"
-    # Obtener un número aleatorio basado en el número de archivos
-    random_number=$(get_random_number "$selected_dir")
+if [ -f "$LAST_SELECTED_FILE" ]; then
+    last_random_number=$(cat "$LAST_SELECTED_FILE")
+else
+    last_random_number=""
+fi
 
-    if [ -n "$random_number" ]; then
-        echo "Número aleatorio: $random_number"
-    fi
+selected_dir="$CARPETA"
+if [ -n "$selected_dir" ]; then
+    random_number=$(get_random_number "$selected_dir")
+    while [ "$random_number" = "$last_random_number" ]; do
+        random_number=$(get_random_number "$selected_dir")
+    done
+    echo "$random_number" > "$LAST_SELECTED_FILE"
 else
     echo "No se encontraron directorios válidos con archivos dentro."
 fi
@@ -42,3 +44,4 @@ echo $CABEZA_ESTADO > /tmp/state.json
 randomascinco=$random_number
 randomascinco=$((randomascinco +5))
 echo ' "title": -1, "type": 5, "currpos": '$random_number', "pagestart": '$random_number', "pageend": '$randomascinco', "emuname": "'$PLATAFORMA'" }] }' >> /tmp/state.json
+
