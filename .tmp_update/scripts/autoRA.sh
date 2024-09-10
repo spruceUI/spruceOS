05e72fcddb5157687d4d7c5cb3e79ace281a805e
spruceOS <177474238+spruceUI@users.noreply.github.com>
2024-09-10 16:32:26 -0700

    Submission from Fragbait79
    This should solve Achievement issues while maintaining a fast boot for non-users.

--- .tmp_update/scripts/autoRA.sh
+++ .tmp_update/scripts/autoRA.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
+check_and_connect_wifi() {
+    if ! ifconfig wlan0 | grep -qE "inet |inet6 "; then
+        ifconfig wlan0 up
+        wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant.conf
+        udhcpc -i wlan0 &
+
+        for i in $(seq 1 15); do
+			# Try to ping retroachievements.org to validate the connection
+			if ping -c 1 -W 1 retroachievements.org >/dev/null 2>&1; then
+				break
+			fi
+            sleep 1
+        done
+    fi
+}
+
 if test -f /mnt/SDCARD/.tmp_update/flags/.save_active; then
+    if grep -q 'cheevos_enable = "true"' /mnt/SDCARD/RetroArch/retroarch.cfg; then
+        check_and_connect_wifi
+    fi
     keymon &
-    /mnt/SDCARD/.tmp_update/flags/.lastgame  &> /dev/null
-    /mnt/SDCARD/.tmp_update/scripts/select.sh  &> /dev/null
-    return
-# else
-#    /mnt/SDCARD/.tmp_update/scripts/principal.sh
+    /mnt/SDCARD/.tmp_update/flags/.lastgame &> /dev/null
+    /mnt/SDCARD/.tmp_update/scripts/select.sh &> /dev/null
 fi
