8cc64c8d1014ef71ee9638d11c5eb57cb7020010
spruceOS <177474238+spruceUI@users.noreply.github.com>
2024-09-13 16:13:12 -0700

    Update expertappswitch.sh

--- App/ExpertAppSwitch/expertappswitch.sh
+++ App/ExpertAppSwitch/expertappswitch.sh
@@ -4,7 +4,7 @@
 BASE_DIR="/mnt/SDCARD/App/"
 
 # List of specific folders to process
-FOLDERS="LEDon BootLogo MiyooGamelist RetroExpert Retroarch sftpgo Syncthing"
+FOLDERS="LEDon BootLogo MiyooGamelist RetroExpert RetroArch sftpgo Syncthing"
 
 # Iterate over each folder name
 for folder in $FOLDERS; do
