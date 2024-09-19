fdb2945ba84423876f312399bfb7ad39d747bac8
Stefan Ayala <stefanayala3266@gmail.com>
2024-09-18 17:13:49 -0700

    Revert "Add files via upload"
    This reverts commit e36c06afb5f895117ec638271f6161745eaf945b.

--- miyoo/app/kill_apps.sh
+++ miyoo/app/kill_apps.sh
@@ -1,22 +1,14 @@
 #!/bin/sh
-echo 30 > /sys/devices/virtual/timed_output/vibrator/enable
-killall keymon &&
-#killall keymon
-brillo=$(cat /sys/devices/virtual/disp/disp/attr/lcdbl)
-echo 0 > /sys/devices/virtual/disp/disp/attr/lcdbl
-pid=$(ps|pgrep retroarch)
-if [ "$pid" -gt 1 ] ; then
-killall runtime.sh
-killall autoRA.sh
-killall principal.sh
-killall -15 retroarch
-sleep 1
-touch  /mnt/SDCARD/.tmp_update/flags/.save_active
-sleep 1
-#show "/mnt/SDCARD/.tmp_update/res/save.png" &
-echo $brillo > /sys/devices/virtual/disp/disp/attr/lcdbl
-/mnt/SDCARD/.tmp_update/scripts/apaga.sh
-else
-poweroff
-echo no retroarch
-fi 
+
+pid=`ps | grep cmd_to_run | grep -v grep | sed 's/[ ]\+/ /g' | cut -d' ' -f2`
+ppid=$pid
+echo pid is $pid
+while [ "" != "$pid" ] ; do
+   ppid=$pid   
+   pid=`pgrep -P $ppid`
+done
+
+if [ "" != "$ppid" ] ; then
+   kill -9 $ppid
+fi
+echo ppid $ppid quit
