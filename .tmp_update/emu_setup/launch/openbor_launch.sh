d3fecfe8928ecb4023761278c64a72ec80fa516b
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-09-24 10:31:00 -0500

    lower conservative governor up_threshold

--- .tmp_update/emu_setup/launch/openbor_launch.sh
+++ .tmp_update/emu_setup/launch/openbor_launch.sh
@@ -15,6 +15,7 @@ echo 1 > /sys/devices/system/cpu/cpu2/online
 echo 1 > /sys/devices/system/cpu/cpu3/online
 echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 30 > /sys/devices/system/cpu/cpufreq/conservative/down_threshold
+echo 70 > /sys/devices/system/cpu/cpufreq/conservative/up_threshold
 echo 3 > /sys/devices/system/cpu/cpufreq/conservative/freq_step
 echo 1 > /sys/devices/system/cpu/cpufreq/conservative/sampling_down_factor
 echo 240000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
