de985ebdff72ce1d028347e61962ec93b38aad87
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-09-24 09:17:24 -0500

    better cpu settings

--- .tmp_update/runtime.sh
+++ .tmp_update/runtime.sh
@@ -113,7 +113,9 @@ log_message "Initial setup scripts executed"
 kill_images
 
 # Initialize CPU/GPU/RAM settings
-/mnt/SDCARD/App/utils/utils "conservative" 4 1344 384 1080 1
+echo 1 > /sys/devices/system/cpu/cpu2/online
+echo 1 > /sys/devices/system/cpu/cpu3/online
+echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo 30 > /sys/devices/system/cpu/cpufreq/conservative/down_threshold
 echo 312000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 
