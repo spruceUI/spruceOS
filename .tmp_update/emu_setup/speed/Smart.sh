3082b1e0588b8922b54c8b27feed8520201a902e
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-09-24 16:49:04 -0500

    replace specific cpu speed scripts with smart/performance/overclock mode scripts

---
+++ .tmp_update/emu_setup/speed/Smart.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+EMU_DIR="$(dirname "$0")"
+CONFIG="$EMU_DIR/config.json"
+SYS_OPT="$EMU_DIR/system.opt"
+
+update_cpu_config_name() {
+    if [ -f "$CONFIG" ]; then
+        sed -i 's|"name": "✓ CPU is set to Smart Mode"|"name": "Set CPU to Smart Mode"|g' "$CONFIG"
+        sed -i 's|"name": "✓ CPU is set to Performance Mode"|"name": "Set CPU to Performance Mode"|g' "$CONFIG"
+        sed -i 's|"name": "✓ CPU is set to Overclock Mode"|"name": "Set CPU to Overclock Mode"|g' "$CONFIG"
+        sed -i 's|"name": "Set CPU to Smart Mode"|"name": "✓ CPU is set to Smart Mode"|g' "$CONFIG"
+    fi
+}
+
+update_cpu_config_name
+
+sed -i 's/GOV=.*/GOV=\"conservative\"/g' "$SYS_OPT"
