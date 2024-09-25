906adc119bbd3fc331f2cf7e28c37b9445329318
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-09-24 19:10:19 -0500

    modify sed calls in cpu mode scripts

--- .tmp_update/emu_setup/speed/Performance.sh
+++ .tmp_update/emu_setup/speed/Performance.sh
@@ -7,6 +7,6 @@ EMU_DIR="/mnt/SDCARD/Emu/$EMU_NAME"
 CONFIG="$EMU_DIR/config.json"
 SYS_OPT="$EMU_DIR/system.opt"
 
-sed -i 's|"name": "CPU Mode".*|"name": "CPU Mode: smart/(PERFORMANCE)/overclock"|g' "$CONFIG"
+sed -i 's|"CPU Mode: (SMART)/performance/overclock"|"CPU Mode: smart/(PERFORMANCE)/overclock"|g' "$CONFIG"
 sed -i 's|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Performance.sh"|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Overclock.sh"|g' "$CONFIG"
 sed -i 's|GOV=.*|GOV=\"performance\"|g' "$SYS_OPT"
--- .tmp_update/emu_setup/speed/Smart.sh
+++ .tmp_update/emu_setup/speed/Smart.sh
@@ -7,6 +7,6 @@ EMU_DIR="/mnt/SDCARD/Emu/$EMU_NAME"
 CONFIG="$EMU_DIR/config.json"
 SYS_OPT="$EMU_DIR/system.opt"
 
-sed -i 's|"name": "CPU Mode".*|"name": "CPU Mode: (SMART)/performance/overclock"|g' "$CONFIG"
+sed -i 's|"CPU Mode: smart/performance/(OVERCLOCK)"|"CPU Mode: (SMART)/performance/overclock"|g' "$CONFIG"
 sed -i 's|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Smart.sh"|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Performance.sh"|g' "$CONFIG"
 sed -i 's|GOV=.*|GOV=\"conservative\"|g' "$SYS_OPT"
