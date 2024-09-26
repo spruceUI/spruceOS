48b46085ba96c402d82185d5fc6ad5b07bbddf2a
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-09-26 10:43:15 -0500

    workaround to prevent ra32.miyoo menu from screwing with governor for too long

--- .tmp_update/emu_setup/launch/standard_launch.sh
+++ .tmp_update/emu_setup/launch/standard_launch.sh
@@ -14,11 +14,7 @@ if [ -f "$OVERRIDE" ]; then
 	. "$OVERRIDE";
 fi
 
-if [ "$GOV" = "overclock" ]; then
-	/mnt/SDCARD/App/utils/utils "performance" 4 1512 384 1080 1
-elif [ "$GOV" = "performance" ]; then
-		/mnt/SDCARD/App/utils/utils "performance" 4 1344 384 1080 1
-else
+set_conservative() {
 	echo 1 > /sys/devices/system/cpu/cpu2/online
 	echo 1 > /sys/devices/system/cpu/cpu3/online
 	echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -29,9 +25,31 @@ else
 	echo 400000 > /sys/devices/system/cpu/cpufreq/conservative/sampling_rate
 	echo 200000 > /sys/devices/system/cpu/cpufreq/conservative/sampling_rate_min
 	echo "$scaling_min_freq" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+}
+
+enforce_conservative() {
+	while true; do
+		sleep 10
+		governor="$(cat "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor")"
+		if [ $governor != "conservative" ]; then
+			set_conservative
+		fi
+	done
+}
+
+if [ "$GOV" = "overclock" ]; then
+	/mnt/SDCARD/App/utils/utils "performance" 4 1512 384 1080 1
+elif [ "$GOV" = "performance" ]; then
+	/mnt/SDCARD/App/utils/utils "performance" 4 1344 384 1080 1
+else
+	set_conservative
+	enforce_conservative &
+	ENFORCE_PID="$!"
 fi
 
 echo $0 $*
 
 cd "$RA_DIR"
 HOME="$RA_DIR/" "$RA_DIR/$RA_BIN" -v -L "$RA_DIR/.retroarch/cores/${CORE}_libretro.so" "$1"
+
+kill -9 "$ENFORCE_PID"
