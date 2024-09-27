6a725b7f65f97bd0e8fd28820ce6edb2ad9c2c3d
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-09-26 23:17:08 -0500

    default N64/DC to Performance mode and disable Smart mode for them

--- .tmp_update/emu_setup/speed/Overclock.sh
+++ .tmp_update/emu_setup/speed/Overclock.sh
@@ -5,6 +5,12 @@ EMU_DIR="/mnt/SDCARD/Emu/${EMU_NAME}"
 CONFIG="$EMU_DIR/config.json"
 SYS_OPT="$EMU_DIR/system.opt"
 
-sed -i 's|"CPU Mode: Smart-(✓PERFORMANCE)-Overclock"|"CPU Mode: Smart-Performance-(✓OVERCLOCK)"|g' "$CONFIG"
-sed -i 's|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Overclock.sh"|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Smart.sh"|g' "$CONFIG"
+if [ "$EMU_NAME" = "DC" ] || [ "$EMU_NAME" = "N64" ]; then
+    sed -i 's|"CPU Mode: (✓PERFORMANCE)-Overclock"|"CPU Mode: Performance-(✓OVERCLOCK)"|g' "$CONFIG"
+    sed -i 's|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Overclock.sh"|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Performance.sh"|g' "$CONFIG"
+else
+    sed -i 's|"CPU Mode: Smart-(✓PERFORMANCE)-Overclock"|"CPU Mode: Smart-Performance-(✓OVERCLOCK)"|g' "$CONFIG"
+    sed -i 's|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Overclock.sh"|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Smart.sh"|g' "$CONFIG"
+fi
+
 sed -i 's|GOV=.*|GOV=\"overclock\"|g' "$SYS_OPT"
--- .tmp_update/emu_setup/speed/Performance.sh
+++ .tmp_update/emu_setup/speed/Performance.sh
@@ -5,6 +5,11 @@ EMU_DIR="/mnt/SDCARD/Emu/${EMU_NAME}"
 CONFIG="$EMU_DIR/config.json"
 SYS_OPT="$EMU_DIR/system.opt"
 
-sed -i 's|"CPU Mode: (✓SMART)-Performance-Overclock"|"CPU Mode: Smart-(✓PERFORMANCE)-Overclock"|g' "$CONFIG"
+if [ "$EMU_NAME" = "DC" ] || [ "$EMU_NAME" = "N64" ]; then
+    sed -i 's|"CPU Mode: Performance-(✓OVERCLOCK)"|"CPU Mode: (✓PERFORMANCE)-Overclock"|g' "$CONFIG"
+else
+    sed -i 's|"CPU Mode: (✓SMART)-Performance-Overclock"|"CPU Mode: Smart-(✓PERFORMANCE)-Overclock"|g' "$CONFIG"
+fi
+
 sed -i 's|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Performance.sh"|"/mnt/SDCARD/.tmp_update/emu_setup/speed/Overclock.sh"|g' "$CONFIG"
 sed -i 's|GOV=.*|GOV=\"performance\"|g' "$SYS_OPT"
