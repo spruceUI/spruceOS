8f2f8a59bc365c5ed0bcadd2141508fe657a6e85
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-09-27 11:28:47 -0500

    add core switch to FDS; simplify core switch for FC/FDS

--- Emu/.emu_setup/core/fceumm.sh
+++ Emu/.emu_setup/core/fceumm.sh
@@ -5,14 +5,6 @@ EMU_DIR="/mnt/SDCARD/Emu/${EMU_NAME}"
 CONFIG="$EMU_DIR/config.json"
 SYS_OPT="$EMU_DIR/system.opt"
 
-update_core_config_name() {
-    if [ -f "$CONFIG" ]; then
-        sed -i 's|"name": "✓ Core is fceumm"|"name": "Change core to fceumm"|g' "$CONFIG"
-        sed -i 's|"name": "✓ Core is nestopia"|"name": "Change core to nestopia"|g' "$CONFIG"
-        sed -i 's|"name": "Change core to fceumm"|"name": "✓ Core is fceumm"|g' "$CONFIG"
-    fi
-}
-
-update_core_config_name
-
-sed -i 's/CORE=.*/CORE=\"fceumm\"/g' "$SYS_OPT"
+sed -i 's|"Emu Core: fceumm-(✓NESTOPIA)"|"Emu Core: (✓FCEUMM)-nestopia"|g' "$CONFIG"
+sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/fceumm.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/nestopia.sh"|g' "$CONFIG"
+sed -i 's|CORE=.*|CORE=\"fceumm\"|g' "$SYS_OPT"
