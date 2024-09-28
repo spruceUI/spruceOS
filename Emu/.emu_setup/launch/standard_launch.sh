17ad98c7efac32dc123783f0c039e7b1f0208844
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-09-27 21:05:32 -0500

    rename functions to better match their... function

--- Emu/.emu_setup/launch/standard_launch.sh
+++ Emu/.emu_setup/launch/standard_launch.sh
@@ -14,7 +14,7 @@ if [ -f "$OVERRIDE" ]; then
 	. "$OVERRIDE";
 fi
 
-set_conservative() {
+set_smart() {
 	echo 1 > /sys/devices/system/cpu/cpu2/online
 	echo 1 > /sys/devices/system/cpu/cpu3/online
 	echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
@@ -27,12 +27,12 @@ set_conservative() {
 	echo "$scaling_min_freq" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
 }
 
-enforce_conservative() {
+enforce_smart() {
 	while true; do
 		sleep 10
 		governor="$(cat "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor")"
 		if [ $governor != "conservative" ]; then
-			set_conservative
+			set_smart
 		fi
 	done
 }
@@ -42,8 +42,8 @@ if [ "$MODE" = "overclock" ]; then
 elif [ "$MODE" = "performance" ]; then
 	/mnt/SDCARD/App/utils/utils "performance" 4 1344 384 1080 1
 else
-	set_conservative
-	enforce_conservative &
+	set_smart
+	enforce_smart &
 	ENFORCE_PID="$!"
 fi
 
