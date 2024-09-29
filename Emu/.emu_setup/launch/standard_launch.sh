1c00e7a00501b7b973aa523888c335c58495e9a0
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-09-29 11:43:24 -0500

    add global logging to standard_launch.sh

--- Emu/.emu_setup/launch/standard_launch.sh
+++ Emu/.emu_setup/launch/standard_launch.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+. /mnt/SDCARD/miyoo/scripts/helperFunctions.sh
+log_message "-----Launching Emulator-----"
+log_message "trying: $0 $@"
 export EMU_NAME="$(echo "$1" | cut -d'/' -f5)"
 export EMU_DIR="/mnt/SDCARD/Emu/${EMU_NAME}"
 export DEF_DIR="/mnt/SDCARD/Emu/.emu_setup/defaults"
@@ -12,7 +15,11 @@ export OVERRIDE="$OVR_DIR/$EMU_NAME/$GAME.opt"
 . "$OPT_DIR/${EMU_NAME}.opt"
 if [ -f "$OVERRIDE" ]; then
 	. "$OVERRIDE";
+	log_message "Launch setting override detected @ $OVERRIDE"
+else
+	log_message "No launch override detected. Using current system settings."
 fi
+
 
 set_smart() {
 	echo 1 > /sys/devices/system/cpu/cpu2/online
@@ -25,14 +32,19 @@ set_smart() {
 	echo 400000 > /sys/devices/system/cpu/cpufreq/conservative/sampling_rate
 	echo 200000 > /sys/devices/system/cpu/cpufreq/conservative/sampling_rate_min
 	echo "$scaling_min_freq" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+	log_message "CPU Mode set to SMART"
 }
 
 set_performance() {
 	/mnt/SDCARD/App/utils/utils "performance" 4 1344 384 1080 1	
+	log_message "CPU Mode set to PERFORMANCE"
+
 }
 
 set_overclock() {
 	/mnt/SDCARD/App/utils/utils "performance" 4 1512 384 1080 1
+	log_message "CPU Mode set to OVERCLOCK"
+
 }
 
 enforce_smart() {
@@ -40,6 +52,7 @@ enforce_smart() {
 		sleep 10
 		governor="$(cat "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor")"
 		if [ $governor != "conservative" ]; then
+			log_message "CPU Mode has changed. Re-enforcing SMART mode"
 			set_smart
 		fi
 	done
@@ -60,3 +73,4 @@ cd "$RA_DIR"
 HOME="$RA_DIR/" "$RA_DIR/$RA_BIN" -v -L "$RA_DIR/.retroarch/cores/${CORE}_libretro.so" "$1"
 
 kill -9 "$ENFORCE_PID"
+log_message "-----Closing Emulator-----"
