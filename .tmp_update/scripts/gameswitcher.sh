b8f97e2aff8f3edbbfc77d498f01a30fec948512
Oscar Kin-Chung Au <oscarkcau@gmail.com>
2024-09-29 17:45:48 +0800

    Add supports of item deletion, instruction and index displaying

--- .tmp_update/scripts/gameswitcher.sh
+++ .tmp_update/scripts/gameswitcher.sh
@@ -58,15 +58,20 @@ while read -r CMD; do
 done <$LIST_FILE
 
 # launch the switcher program
-# Usage: switcher image_list title_list [-s speed] [-m on|off] [-t on|off] [-ts speed]
+# Usage: switcher image_list title_list [-s speed] [-b on|off] [-m on|off] [-t on|off] [-ts speed] [-n on|off] [-d command]
 # -s: scrolling speed in frames (default is 20), larger value means slower.
+# -b: swap left/right buttons for image scrolling (default is off).
 # -m: display title in multiple lines (default is off).
 # -t: display title at start (default is on).
 # -ts: title scrolling speed in pixel per frame (default is 4).
+# -n: display item index (default is on).
+# -d: enable item deletion with the deletion command provided (default is disable).
+#     Use TITLE in command to take the selected title as input. e.g. "echo TITLE"
+#     Pass "" as argument if no command is provided.
 # -h,--help show this help message.
 # return value: the 1-based index of the selected image
 cd /mnt/SDCARD/.tmp_update/bin/
-/mnt/SDCARD/.tmp_update/bin/switcher "$IMAGES_FILE" "$GAMENAMES_FILE" -s 10
+/mnt/SDCARD/.tmp_update/bin/switcher "$IMAGES_FILE" "$GAMENAMES_FILE" -s 10 -d "sed -i '/TITLE/c\removed' $LIST_FILE"
 
 # get return value and launch game with return index
 RETURN_INDEX=$?
@@ -74,9 +79,9 @@ if [ $RETURN_INDEX -gt 0 ]; then
     # get command that launches the game
     CMD=`tail -n+$RETURN_INDEX "$LIST_FILE" | head -1`
 
-    # move the selected game to the end of the list file
-    # 1. get all commands except the selected game
-    grep -Fxv "$CMD" "$LIST_FILE" > "$TEMP_FILE"
+    # move the selected game to the end of the list file & skip all removed items
+    # 1. get all commands except the selected game or removed lines
+    grep -Fxv "$CMD" "$LIST_FILE" | grep -Fxv "removed" > "$TEMP_FILE"
     mv "$TEMP_FILE" "$LIST_FILE"
     # 2. append the command for current game to the end of game list file 
     echo "$CMD" >> "$LIST_FILE"
@@ -84,4 +89,8 @@ if [ $RETURN_INDEX -gt 0 ]; then
     # wrtie command to file which will be run by principle.sh
     echo $CMD > /tmp/cmd_to_run.sh
     sync
+else
+    # skip all removed items
+    grep -Fxv "removed" "$LIST_FILE" > "$TEMP_FILE"
+    mv "$TEMP_FILE" "$LIST_FILE"
 fi
