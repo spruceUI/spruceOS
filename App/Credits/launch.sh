e6fcd8797ae8e096e7775c4afb25314aa86c3f5c
cinethezs <cinethezs@proton.me>
2024-10-03 23:18:57 +0200

    change font to nunwen

--- App/Credits/launch.sh
+++ App/Credits/launch.sh
@@ -3,4 +3,4 @@
 /mnt/SDCARD/App/Credits/sprucepixelbg43.png \
 /mnt/SDCARD/App/Credits/credits.txt \
 /mnt/SDCARD/App/Credits/kill-screen-194698.mp3 \
-/mnt/SDCARD/App/Credits/prstartk.ttf 22
+/mnt/SDCARD/Themes/SPRUCE/nunwen.ttf 22
