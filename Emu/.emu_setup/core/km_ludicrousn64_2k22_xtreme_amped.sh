2879817dc80fb78029de6ade96e3629172896df6
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-10-07 10:59:38 -0500

    update ludicrousn64 2k22 core
    Co-authored-by: Kyland K <kkainbryce@oh.rr.com>

--- Emu/.emu_setup/core/km_ludicrousn64_2k22_xtreme_amped.sh
+++ Emu/.emu_setup/core/km_ludicrousn64_2k22_xtreme_amped.sh
@@ -5,5 +5,5 @@ CONFIG="/mnt/SDCARD/Emu/${EMU_NAME}/config.json"
 SYS_OPT="/mnt/SDCARD/Emu/.emu_setup/options/${EMU_NAME}.opt"
 
 sed -i 's|"Emu Core:ludicrousn64-(✓MUPEN64PLUS)"|"Emu Core: (✓LUDICROUSN64)-mupen64plus"|g' "$CONFIG"
-sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/km_ludicrousn64_xtreme_amped.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/mupen64plus.sh"|g' "$CONFIG"
-sed -i 's|CORE=.*|CORE=\"km_ludicrousn64_xtreme_amped\"|g' "$SYS_OPT"
+sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/km_ludicrousn64_2k22_xtreme_amped.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/mupen64plus.sh"|g' "$CONFIG"
+sed -i 's|CORE=.*|CORE=\"km_ludicrousn64_2k22_xtreme_amped\"|g' "$SYS_OPT"
--- Emu/.emu_setup/core/mupen64plus.sh
+++ Emu/.emu_setup/core/mupen64plus.sh
@@ -5,5 +5,5 @@ CONFIG="/mnt/SDCARD/Emu/${EMU_NAME}/config.json"
 SYS_OPT="/mnt/SDCARD/Emu/.emu_setup/options/${EMU_NAME}.opt"
 
 sed -i 's|"Emu Core: (✓LUDICROUSN64)-mupen64plus"|"Emu Core:ludicrousn64-(✓MUPEN64PLUS)"|g' "$CONFIG"
-sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/mupen64plus.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/km_ludicrousn64_xtreme_amped.sh"|g' "$CONFIG"
+sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/mupen64plus.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/km_ludicrousn64_2k22_xtreme_amped.sh"|g' "$CONFIG"
 sed -i 's|CORE=.*|CORE=\"mupen64plus\"|g' "$SYS_OPT"
