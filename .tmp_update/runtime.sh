cef229aa9badcd8d583b6f85c03ac535404c7eb0
spruceOS <177474238+spruceUI@users.noreply.github.com>
2024-10-09 16:47:07 -0700

    Update runtime.sh

--- .tmp_update/runtime.sh
+++ .tmp_update/runtime.sh
@@ -121,7 +121,7 @@ log_message "Swap file activated"
 # Run scripts for initial setup
 /mnt/SDCARD/.tmp_update/scripts/forcedisplay.sh
 /mnt/SDCARD/.tmp_update/scripts/low_power_warning.sh
-/mnt/SDCARD/.tmp_update/scripts/ffplay_is_now_media.sh
+/mnt/SDCARD/spruce/scripts/ffplay_is_now_media.sh
 /mnt/SDCARD/.tmp_update/scripts/checkfaves.sh &
 log_message "Initial setup scripts executed"
 kill_images
