5385c818ac4f4e1dd683790c06dd8d672da04887
spruceOS <177474238+spruceUI@users.noreply.github.com>
2024-10-08 20:14:37 -0700

    Delete Emu/VIDEOS directory

--- Emu/VIDEOS/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mydir=`dirname "$0"`
-
-export HOME=$mydir
-export PATH=$mydir/bin:$PATH
-export LD_LIBRARY_PATH=$mydir/libs:/usr/miyoo/lib:/usr/lib:$LD_LIBRARY_PATH
-
-cd $mydir
-ffplay -vf transpose=2 -fs -i "$1"
