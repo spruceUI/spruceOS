064607690e3d6a5e53a6cb85ab6b47f966e1ab44
XanXic <XanderXic@gmail.com>
2024-10-09 20:41:20 -0400

    Adding recursion and file check to ffplay_is_now_media

--- spruce/scripts/ffplay_is_now_media.sh
+++ spruce/scripts/ffplay_is_now_media.sh
@@ -6,7 +6,27 @@ OLD_DIR="/mnt/SDCARD/Roms/FFPLAY"
 NEW_DIR="/mnt/SDCARD/Roms/MEDIA"
 
 if [ -d "$OLD_DIR" ]; then
-    mv "$OLD_DIR"/* "$NEW_DIR"/
-    rmdir “$OLD_DIR”
-    log_message "Found $OLD_DIR folder; moved contents of $OLD_DIR to $NEW_DIR"
+    mkdir -p "$NEW_DIR"
+    
+    # Move all contents, including hidden files and subdirectories
+    cd "$OLD_DIR" || exit 1
+    find . -mindepth 1 -exec sh -c '
+        for item do
+            if [ -e "$item" ]; then
+                dir=$(dirname "$item")
+                mkdir -p "'"$NEW_DIR"'/$dir"
+                mv "$item" "'"$NEW_DIR"'/$item"
+            fi
+        done
+    ' sh {} +
+    
+    # Check if the move was successful
+    if [ $? -eq 0 ]; then
+        log_message "Contents of $OLD_DIR successfully moved to $NEW_DIR"
+        cd ..
+        rm -rf "$OLD_DIR"
+        if [ $? -eq 0 ]; then
+            log_message "$OLD_DIR folder and any remaining contents removed"
+        fi
+    fi
 fi
