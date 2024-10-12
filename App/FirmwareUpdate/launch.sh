7729d030945996374fc17bb03117c64a7dc43d78
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-10-11 20:12:26 -0500

    separate charging and capacity checks; loop while not charging unless B pressed

--- App/FirmwareUpdate/launch.sh
+++ App/FirmwareUpdate/launch.sh
@@ -14,9 +14,10 @@ VERSION="$(cat /usr/miyoo/version)"
 
 cancel_update() {
 	display -o -t "Firmware update cancelled."
+	log_message "User cancelled FW update."
 	if [ -f "/mnt/SDCARD/miyoo282_fw.img" ]; then
 		rm "/mnt/SDCARD/miyoo282_fw.img"
-		log_message "User cancelled FW update. Removing FW image from root of card."
+		log_message "Removing FW image from root of card."
 	fi
 	exit 1
 }
@@ -34,30 +35,34 @@ confirm_update() {
 }
 
 if [ "$VERSION" -ge 20240713100458 ]; then
-	display -d 3 -t "Firmware is up to date - happy gaming!!!!!!!!!!"
+	display -o -t "Firmware is up to date - happy gaming!!!!!!!!!!"
     sed -i 's|"label":|"#label":|' "$CONFIG"
 	exit 0
 fi
 
 if [ "$FREE_SPACE" -lt 64 ]; then
-	display -d 3 -t "Not enough free space. Please ensure at least 64 MiB of space is available on your SD card, then try again."
+	display -o -t "Not enough free space. Please ensure at least 64 MiB of space is available on your SD card, then try again."
 	exit 1
 fi
 
-if [ "$CHARGING" -eq 1 ] && [ "$CAPACITY" -ge 20 ]; then
+if [ "$CAPACITY" -lt 10 ]; then
+	display -o -t "As a precaution, please charge your A30 to at least 10% capacity, then try again."
+	exit 1
+fi
 
-	display -t "A firmware update is ready for your device. The spruce team highly recommends that you proceed with the update; however, please be aware that if interrupted before the update is complete, it could temporarily brick your device, requiring you to run the unbricker software. Press B to cancel the update, or press SELECT to continue."
+while [ "$CHARGING" -eq 0 ]; do
+	display -t "A firmware update is ready for your device. Before continuing, please connect your device to a power source, then press A to proceed. If you do not wish to update, press B instead."
 	B_pressed=0
-	SE_pressed=0
+	A_pressed=0
 	get_event | while read input; do
 		case "$input" in 
 			*"$B_B 1"*)
 				B_pressed=1
 				log_message "B pressed"
 				;;
-			*"$B_SELECT 1"*)
-				SE_pressed=1
-				log_message "SELECT pressed"
+			*"$B_A 1"*)
+				A_pressed=1
+				log_message "A pressed"
 				;;
 		esac
 		if [ "$B_pressed" = 1 ]; then
@@ -65,15 +70,38 @@ if [ "$CHARGING" -eq 1 ] && [ "$CAPACITY" -ge 20 ]; then
 			log_message "cancelling update."
 			cancel_update
 			break
-		elif [ "$SE_pressed" = 1 ]; then
+		elif [ "$A_pressed" = 1 ]; then
 			killall getevent
-			log_message "confirming update."
-			confirm_update
 			break
 		fi
 	done
+	display_kill
+done
 
-else
-	display -d 3 -t "Please plug in your device and allow it to charge above 20%, then try again."
-	exit 1
-fi
+display -t " The spruce team highly recommends that you proceed with the update; however, please be aware that if interrupted before the update is complete, it could temporarily brick your device, requiring you to run the unbricker software. Press B to cancel the update, or press SELECT to continue."
+B_pressed=0
+SE_pressed=0
+get_event | while read input; do
+	case "$input" in 
+		*"$B_B 1"*)
+			B_pressed=1
+			log_message "B pressed"
+			;;
+		*"$B_SELECT 1"*)
+			SE_pressed=1
+			log_message "SELECT pressed"
+			;;
+	esac
+	if [ "$B_pressed" = 1 ]; then
+		killall getevent
+		log_message "cancelling update."
+		cancel_update
+		break
+	elif [ "$SE_pressed" = 1 ]; then
+		killall getevent
+		log_message "confirming update."
+		confirm_update
+		break
+	fi
+done
+killall getevent
