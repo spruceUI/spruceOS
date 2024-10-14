46389580ce282f0474f083041e0c800b69151c08
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-10-14 09:58:56 -0500

    make enforceSmartCPU... smarter

--- spruce/scripts/enforceSmartCPU.sh
+++ spruce/scripts/enforceSmartCPU.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 . "/mnt/SDCARD/spruce/scripts/helperFunctions.sh"
+set_smart # this should be the starting point before trying to detect a change
 GOVERNOR_FILE="/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
 while true; do
     /mnt/SDCARD/.tmp_update/bin/inotify.elf "$GOVERNOR_FILE"
