447ac5dbd35072698a09ed13e70d5168635b38bd
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-10-15 14:17:14 -0500

    Revert "framework to accept a single dir to refresh as an argument to the script"
    This reverts commit 7bc4e724a91025d2d4d22eaa56a77526063937aa.

--- spruce/scripts/emufresh_new.sh
+++ spruce/scripts/emufresh_new.sh
@@ -25,14 +25,18 @@ has_roms() {
     search_dir="$ROM_DIR/$system_name"
     json_file="$1/config.json"
     extensions="$(jq -r '.extlist' "$json_file" | tr '|' ' ')"
+
+
     if [ ! -f "$json_file" ]; then
         return 1
     fi
+
     for ext in $extensions; do
         if find "$search_dir" -type f -name "*.$ext" | grep -q .; then
             return 0
         fi
     done
+
     return 1
 }
 
@@ -48,52 +52,28 @@ unhide_system() {
     fi
 }
 
-refresh_all_emus() {
-    if [ -f "$show_img" ]; then
-        show "$show_img" &
-    fi
-    delete_cache_files
-    for dir in "$EMU_DIR"/*; do
-        if [ -d "$dir" ]; then
-            if has_roms "$dir"; then
-                unhide_system "$dir"
-            else
-                hide_system "$dir"
-            fi
-        fi
-    done
-}
-
-refresh_emu() {
-    find "$1" -name "*cache6.db" -exec rm {} \;
-    if [ -d "$1" ]; then
-        if has_roms "$1"; then
-            unhide_system "$1"
-        else
-            hide_system "$1"
-        fi
-    fi
-    if [ "$1" = ]
-}
-
-refresh_p8() {
-    find "$$P8_DIR" -name "*cache6.db" -exec rm {} \;
-    if [ -f "$P8_DIR/bin/pico8.dat" ] && [ -f "$P8_DIR/bin/pico8_dyn" ]; then
-        unhide_system "$P8_DIR"
-    else
-        hide_system "$P8_DIR"
-    fi
-}
-
 ##### MAIN EXECUTION #####
 
-if [ "$1" = "$P8_DIR" ]; then
-    refresh_p8
-elif [ "$1" = "" ]; then
-    refresh_all_emus
-    refresh_p8
+if [ -f "$show_img" ]; then
+    show "$show_img" &
+fi
+
+delete_cache_files
+
+for dir in "$EMU_DIR"/*; do
+    if [ -d "$dir" ]; then
+        if has_roms "$dir"; then
+            unhide_system "$dir"
+        else
+            hide_system "$dir"
+        fi
+    fi
+done
+
+if [ -f "$P8_DIR/bin/pico8.dat" ] && [ -f "$P8_DIR/bin/pico8_dyn" ]; then
+    unhide_system "$P8_DIR"
 else
-    refresh_emu "$1"
+    hide_system "$P8_DIR"
 fi
 
 killall -9 show
