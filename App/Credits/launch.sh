2849fc64c885550efb3ccb6b554133ae26f6b6e2
Sundownersport <kilchisblue@aol.com>
2024-10-15 15:53:01 -0700

    Add files via upload

--- App/Credits/launch.sh
+++ App/Credits/launch.sh
@@ -2,5 +2,5 @@
 /mnt/SDCARD/App/Credits/credits_app.elf \
 /mnt/SDCARD/App/Credits/sprucepixelbg43.png \
 /mnt/SDCARD/App/Credits/credits.txt \
-/mnt/SDCARD/App/Credits/kill-screen-194698.mp3 \
+"/mnt/SDCARD/App/Credits/Sweater Ass Sounding Ass.mp3" \
 /mnt/SDCARD/Themes/SPRUCE/nunwen.ttf 22
