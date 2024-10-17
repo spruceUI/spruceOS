491b5b5992216554653d465bafc8f29934b4896a
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-10-16 20:49:57 -0500

    tidying up

--- spruce/scripts/firstboot.sh
+++ spruce/scripts/firstboot.sh
@@ -6,14 +6,17 @@ SETTINGS_FILE="/config/system.json"
 SWAPFILE="/mnt/SDCARD/cachefile"
 SDCARD_PATH="/mnt/SDCARD"
 
-IMAGE_PATH_WIKI="${SDCARD_PATH}/.tmp_update/res/wiki.png"
-FW_ICON="${SDCARD_PATH}/Themes/SPRUCE/icons/App/firmwareupdate.png"
-IMAGE_PATH_ENJOY="${SDCARD_PATH}/.tmp_update/res/enjoy.png"
+BG_IMAGE="/mnt/SDCARD/spruce/imgs/displayTextPreColor.png"
+SPRUCE_LOGO="/mnt/SDCARD/spruce/imgs/spruce_logo.png"
+FW_ICON="/mnt/SDCARD/Themes/SPRUCE/icons/App/firmwareupdate.png"
+ICONFRESH_ICON="/mnt/SDCARD/Themes/SPRUCE/App/iconfresh.png"
+WIKI_ICON="/mnt/SDCARD/spruce/imgs/book.png"
+HAPPY_ICON="/mnt/SDCARD/spruce/imgs/smile.png"
 
 log_message "Starting firstboot script"
 
 if flag_check "first_boot"; then
-    display -i "/mnt/SDCARD/spruce/imgs/displayTextPreColor.png" --icon "/mnt/SDCARD/spruce/imgs/spruce_logo.png" -t "Installing spruce v3.0.0!" -p bottom
+    display -i "$BG_IMAGE" --icon "$SPRUCE_LOGO" -t "Installing spruce v3.0.0!" -p bottom
     log_message "First boot flag detected"
     
     # don't overwrite user's config if it's not a TRUE first boot
@@ -49,20 +52,20 @@ if flag_check "first_boot"; then
     /mnt/SDCARD/spruce/scripts/emufresh_md5_multi.sh
     
     log_message "Running iconfresh.sh"
-    display -p bottom -t "Refreshing icons... please wait......" -i "/mnt/SDCARD/spruce/imgs/displayTextPreColor.png" --icon "/mnt/SDCARD/Themes/SPRUCE/App/iconfresh.png"
+    display -p bottom -t "Refreshing icons... please wait......" -i "$BG_IMAGE" --icon "$ICONFRESH_ICON"
     /mnt/SDCARD/spruce/scripts/iconfresh.sh --silent
 
     log_message "Displaying wiki image"
-    display -d 5 -i "/mnt/SDCARD/spruce/imgs/displayTextPreColor.png" --icon "/mnt/SDCARD/spruce/imgs/book.png" -p bottom -t "Check out the spruce wiki on our GitHub page for tips and FAQs!"
+    display -d 5 -i "$BG_IMAGE" --icon "$WIKI_ICON" -p bottom -t "Check out the spruce wiki on our GitHub page for tips and FAQs!"
 
     VERSION=$(cat /usr/miyoo/version)
     if [ "$VERSION" -lt 20240713100458 ]; then
         log_message "Detected firmware version $VERSION, suggesting update"
-        display -i "/mnt/SDCARD/spruce/imgs/displayTextPreColor.png" --icon "/mnt/SDCARD/Themes/SPRUCE/Icons/App/firmwareupdate.png" -d 5 -p bottom -t "Visit the App section from the main menu to update your firmware to the latest version. It fixes the A30's Wi-Fi issues!"
+        display -i "$BG_IMAGE" --icon "$FW_ICON" -d 5 -p bottom -t "Visit the App section from the main menu to update your firmware to the latest version. It fixes the A30's Wi-Fi issues!"
     fi
     
     log_message "Displaying enjoy image"
-    display -d 5 -i "/mnt/SDCARD/spruce/imgs/displayTextPreColor.png" --icon "/mnt/SDCARD/spruce/imgs/smile.png" -p bottom -t "Happy gaming..........!"
+    display -d 5 -i "$BG_IMAGE" --icon "$HAPPY_ICON" -p bottom -t "Happy gaming..........!"
 
     flag_remove "first_boot"
     log_message "Removed first boot flag"
