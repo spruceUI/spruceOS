630590b5b74e76c6f5a457f311c3e6a8aa718690
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-10-17 19:06:09 -0500

    new bg image; wifi on by default after upgrade

--- spruce/scripts/firstboot.sh
+++ spruce/scripts/firstboot.sh
@@ -6,7 +6,7 @@ SETTINGS_FILE="/config/system.json"
 SWAPFILE="/mnt/SDCARD/cachefile"
 SDCARD_PATH="/mnt/SDCARD"
 
-BG_IMAGE="/mnt/SDCARD/spruce/imgs/displayTextPreColor.png"
+BG_IMAGE="/mnt/SDCARD/spruce/imgs/bg_tree.png"
 SPRUCE_LOGO="/mnt/SDCARD/spruce/imgs/spruce_logo.png"
 FW_ICON="/mnt/SDCARD/Themes/SPRUCE/icons/App/firmwareupdate.png"
 ICONFRESH_ICON="/mnt/SDCARD/Themes/SPRUCE/App/iconfresh.png"
@@ -16,17 +16,12 @@ HAPPY_ICON="/mnt/SDCARD/spruce/imgs/smile.png"
 log_message "Starting firstboot script"
 
 if flag_check "first_boot"; then
+
+    # initialize the settings... users can restore their own backup later.
+    cp "${SDCARD_PATH}/.tmp_update/system.json" "$SETTINGS_FILE" && sync
+
     display -i "$BG_IMAGE" --icon "$SPRUCE_LOGO" -t "Installing spruce v3.0.0!" -p bottom
     log_message "First boot flag detected"
-    
-    # don't overwrite user's config if it's not a TRUE first boot
-    if ! flag_check "config_copied"; then
-        cp "${SDCARD_PATH}/.tmp_update/system.json" "$SETTINGS_FILE"
-        flag_add "config_copied"
-        sync
-        sleep 5  ### why is this here? -Ry
-        log_message "Copied system.json and created config_copied.lock"
-    fi
     
     if [ -f "${SWAPFILE}" ]; then
         SWAPSIZE=$(du -k "${SWAPFILE}" | cut -f1)
@@ -46,7 +41,7 @@ if flag_check "first_boot"; then
     fi
     
     log_message "Running emu_setup.sh"
-    /mnt/SDCARD/Emu/.emu_setup/emu_setup.sh &
+    /mnt/SDCARD/Emu/.emu_setup/emu_setup.sh
     
     log_message "Running emufresh.sh"
     /mnt/SDCARD/spruce/scripts/emufresh_md5_multi.sh
