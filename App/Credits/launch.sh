b8310d112d99c1b0b2ab2b678dd2443f8a90050e
cinethezs <cinethezs@proton.me>
2024-10-19 20:08:42 +0200

    multi audio files option and loop over + color font to match spruce theme

--- App/Credits/launch.sh
+++ App/Credits/launch.sh
@@ -2,5 +2,6 @@
 /mnt/SDCARD/App/Credits/credits_app.elf \
 /mnt/SDCARD/App/Credits/sprucepixelbg43.png \
 /mnt/SDCARD/App/Credits/credits.txt \
+/mnt/SDCARD/Themes/SPRUCE/nunwen.ttf 22 \
 "/mnt/SDCARD/App/Credits/Sweater Ass Sounding Ass.mp3" \
-/mnt/SDCARD/Themes/SPRUCE/nunwen.ttf 22
+"/mnt/SDCARD/App/Credits/3 - Sir Daniel Bonaduce.mp3"
