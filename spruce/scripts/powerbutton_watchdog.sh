234fc0292bf575f5773ffceb0f7efd1ac5d68340
Oscar Kin-Chung Au <oscarkcau@gmail.com>
2024-10-20 02:38:19 +0800

    new logic for powerbutton watchdog to prevent RA menu pop up when pressing power button

--- spruce/scripts/powerbutton_watchdog.sh
+++ spruce/scripts/powerbutton_watchdog.sh
@@ -50,24 +50,22 @@ long_press_handler() {
     echo heartbeat > /sys/devices/platform/sunxi-led/leds/led1/trigger
     vibrate
 
-    # show saving screen
-    show_image "/mnt/SDCARD/.tmp_update/res/save.png"
-
-    # kill principle first so no new app / MainUI will be loaded anymore
+    # kill principle and runtime first so no new app / MainUI will be loaded anymore
+    killall -q -15 runtime.sh
     killall -q -15 principal.sh
 
     # kill enforceSmartCPU first so no CPU setting is changed during shutdown
-    killall -q -15 enforceSmartCPU.sh                
+    killall -q -15 enforceSmartCPU.sh
 
     # trigger auto save and send kill signal 
     if pgrep "ra32.miyoo" > /dev/null ; then
-        {
-            echo 1 1 0   # MENU up
-            echo 1 57 1  # A down
-            echo 1 57 0  # A up
-            echo 0 0 0   # tell sendevent to exit
-        } | $BIN_PATH/sendevent /dev/input/event3
-        sleep 0.3
+        # {
+        #     echo 1 1 0   # MENU up
+        #     echo 1 57 1  # A down
+        #     echo 1 57 0  # A up
+        #     echo 0 0 0   # tell sendevent to exit
+        # } | $BIN_PATH/sendevent /dev/input/event3
+        # sleep 0.3
         killall -q -15 ra32.miyoo
     elif pgrep "PPSSPPSDL" > /dev/null ; then
         {
@@ -84,9 +82,6 @@ long_press_handler() {
         killall -q -15 drastic
         killall -q -9 MainUI
     fi
-
-    # show saving screen
-    show_image "/mnt/SDCARD/.tmp_update/res/save.png"
 
     # wait until emulator or MainUI exit 
     while killall -q -0 ra32.miyoo || \
@@ -115,75 +110,76 @@ long_press_handler() {
 flag_remove "pb.longpress"
 flag_remove "pb.sleep"
 
-# listen to event0 and handle key press events
-$BIN_PATH/getevent /dev/input/event0 | while read line; do
-    case $line in
-        *"key 1 116 1"*) # MENU key down
-            # not in previous sleep event 
-            if ! flag_check "pb.sleep" ; then
-                # start long press handler
-                long_press_handler &
-                PID=$!
+while true ; do
 
-            # delete sleep flag, now ready for sleep again
-            else
-                flag_remove "pb.sleep"            
-            fi
-        ;;
-        *"key 1 116 0"*) # MENU key up
-            # if NOT long press
-            if flag_check "pb.longpress" ; then
-                # kill long press handler and remove flag
-                kill $PID
-                flag_remove "pb.longpress"
+    # listen to event0 and handle key press events
+    $BIN_PATH/getevent /dev/input/event0 -exclusive | while read line; do
+        case $line in
+            *"key 1 116 1"*) # MENU key down
+                # not in previous sleep event 
+                if ! flag_check "pb.sleep" && ! flag_check "pb.longpress" ; then
+                    # start long press handler
+                    long_press_handler &
+                    PID=$!
+                fi
+            ;;
+            *"key 1 116 0"*) # MENU key up
+                # if NOT long press
+                if flag_check "pb.longpress" ; then
+                    # kill long press handler and remove flag
+                    kill $PID
+                    flag_remove "pb.longpress"
 
-                # add sleep flag
-                flag_add "pb.sleep"
+                    # add sleep flag
+                    flag_add "pb.sleep"
 
-                # PAUSE pany process that may crash the system during wakeup
-                killall -q -19 enforceSmartCPU.sh                
+                    # PAUSE pany process that may crash the system during wakeup
+                    killall -q -19 enforceSmartCPU.sh
 
-                sleep 0.5
+                    # PAUSE any other running emulator or MainUI
+                    killall -q -19 ra32.miyoo || \
+                    killall -q -19 retroarch || \
+                    killall -q -19 PPSSPPSDL || \
+                    killall -q -19 drastic || \
+                    killall -q -19 MainUI
 
-                # PAUSE any running emulator or MainUI
-                killall -q -19 ra32.miyoo
-                killall -q -19 retroarch
-                killall -q -19 PPSSPPSDL
-                killall -q -19 drastic
-                killall -q -19 MainUI
+                    // kill getevent program, prepare to break inner while loop
+                    kill $(pgrep -f "getevent /dev/input/event0 -exclusive")
+                    sleep 0.5
 
-                sleep 0.5
+                    // now break inner while loop
+                    break
+                fi
+            ;;
+        esac
+    done 
 
-                # ensure all cache is written to SD card 
-                sync
-                
-                # suspend to memory
-                echo -n mem > /sys/power/state
-            fi
-        ;;
-        *"key 1 143 0"*) # Wakeup 
-            # wait long enough to handle any wakeup step, including restore wifi  
-            sleep 3
-            
-            # RESUME any running emulator or MainUI
-            killall -q -18 ra32.miyoo
-            killall -q -18 retroarch
-            killall -q -18 PPSSPPSDL
-            killall -q -18 drastic
-            killall -q -18 MainUI
+    # ensure all cache is written to SD card 
+    sync
 
-            # wait long enough for emulator to resume 
-            sleep 2
+    # suspend to memory
+    echo -n mem > /sys/power/state
 
-            # update display setting after wakeup
-            ENHANCE_SETTINGS=$(cat /sys/devices/virtual/disp/disp/attr/enhance)
-            echo "$ENHANCE_SETTINGS" > /sys/devices/virtual/disp/disp/attr/enhance
+    # wait long enough to ensure device enter sleep mode
+    sleep 1
 
-            # RESUME any process that may crash the system during wakeup
-            killall -q -18 enforceSmartCPU.sh
+    # update display setting after wakeup
+    ENHANCE_SETTINGS=$(cat /sys/devices/virtual/disp/disp/attr/enhance)
+    echo "$ENHANCE_SETTINGS" > /sys/devices/virtual/disp/disp/attr/enhance
 
-            # delete sleep flag, now ready for sleep again
-            flag_remove "pb.sleep"
-        ;;
-    esac
-done 
+    # wait long enough to ensure wakeup task is finished
+    sleep 2
+
+    # RESUME any running emulator or MainUI
+    killall -q -18 ra32.miyoo || \
+    killall -q -18 retroarch || \
+    killall -q -18 PPSSPPSDL || \
+    killall -q -18 drastic || \
+    killall -q -18 MainUI
+
+    # RESUME any process that may crash the system during wakeup
+    killall -q -18 enforceSmartCPU.sh
+
+    # delete sleep flag, now ready for sleep again
+    flag_remove "pb.sleep"
+done
