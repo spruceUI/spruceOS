dd65c7cca44367b46476fb4f216194ce6eff6f2c
XanXic <XanderXic@gmail.com>
2024-10-20 17:59:49 -0400

    Cleaning up save_power.sh, using new confirm method and prompts

--- spruce/scripts/save_poweroff.sh
+++ spruce/scripts/save_poweroff.sh
@@ -5,61 +5,43 @@ BIN_PATH="/mnt/SDCARD/.tmp_update/bin"
 FLAGS_DIR="/mnt/SDCARD/spruce/flags"
 
 kill_current_process() {
-    pid=$(ps | grep cmd_to_run | grep -v grep | sed 's/[ ]\+/ /g' | cut -d' ' -f2)
-    ppid=$pid
-    while [ "" != "$pid" ]; do
-        ppid=$pid
-        pid=$(pgrep -P $ppid)
-    done
+	pid=$(ps | grep cmd_to_run | grep -v grep | sed 's/[ ]\+/ /g' | cut -d' ' -f2)
+	ppid=$pid
+	while [ "" != "$pid" ]; do
+		ppid=$pid
+		pid=$(pgrep -P $ppid)
+	done
 
-    if [ "" != "$ppid" ]; then
-        kill -9 $ppid
-    fi
+	if [ "" != "$ppid" ]; then
+		kill -9 $ppid
+	fi
 }
 
 vibrate
 
 # ask for user response if MainUI is running
-if flag_check "in_menu" ; then
-    messages_file="/var/log/messages"
+if flag_check "in_menu"; then
+	messages_file="/var/log/messages"
 	# pause MainUI
 	killall -q -19 MainUI
 	# show notification screen
-	display --text "Are you sure you want to shutdown?
- A-Confirm B-Cancel" --image "/mnt/SDCARD/spruce/imgs/bg_tree.png"
-	# wait for button input
-    while true; do
-		# wait for log message update
-        inotifywait "$messages_file"
-		# get the last line of log file
-        last_line=$(tail -n 1 "$messages_file")
-        case "$last_line" in
-			# B button - cancel shutdown
-			*"key 1 29"*)
-				# dismiss notification screen
-				display_kill
-				# resume Mainui
-				killall -q -18 MainUI
-				# exit script
-				return 0
-				break
-				;;
-			# A button - confirm shutdown
-			*"key 1 57"*) 
-				# dismiss notification screen
-				display_kill
-				# remove lastgame flag to prevent loading any App after next boot
-				rm "${FLAGS_DIR}/lastgame.lock"
-				# turn off screen
-				echo 0 > /sys/devices/virtual/disp/disp/attr/lcdbl
-				break
-				;;
-        esac
-    done
+	display --text "Are you sure you want to shutdown?" --image "/mnt/SDCARD/spruce/imgs/bg_tree.png" --confirm
+	if confirm; then
+		# remove lastgame flag to prevent loading any App after next boot
+		rm "${FLAGS_DIR}/lastgame.lock"
+		# turn off screen
+		echo 0 >/sys/devices/virtual/disp/disp/attr/lcdbl
+	else
+		display_kill
+		# resume Mainui
+		killall -q -18 MainUI
+		# exit script
+		return 0
+	fi
 fi
 
-# notify user with vibration and led 
-echo heartbeat > /sys/devices/platform/sunxi-led/leds/led1/trigger
+# notify user with vibration and led
+echo heartbeat >/sys/devices/platform/sunxi-led/leds/led1/trigger
 
 # kill principle and runtime first so no new app / MainUI will be loaded anymore
 killall -q -15 runtime.sh
@@ -68,20 +50,20 @@ killall -q -15 principal.sh
 # kill enforceSmartCPU first so no CPU setting is changed during shutdown
 killall -q -15 enforceSmartCPU.sh
 
-# kill app if not emulator is running 
-if cat /tmp/cmd_to_run.sh | grep -q -v '/mnt/SDCARD/Emu' ; then
+# kill app if not emulator is running
+if cat /tmp/cmd_to_run.sh | grep -q -v '/mnt/SDCARD/Emu'; then
 	kill_current_process
 	# remove lastgame flag to prevent loading any App after next boot
 	rm "${FLAGS_DIR}/lastgame.lock"
 fi
 
 # kill PICO8 if PICO8 is running
-if pgrep "pico8_dyn" > /dev/null; then
+if pgrep "pico8_dyn" >/dev/null; then
 	killall -q -15 pico8_dyn
 fi
 
-# trigger auto save and send kill signal 
-if pgrep "ra32.miyoo" > /dev/null ; then
+# trigger auto save and send kill signal
+if pgrep "ra32.miyoo" >/dev/null; then
 	# {
 	#     echo 1 1 0   # MENU up
 	#     echo 1 57 1  # A down
@@ -90,13 +72,13 @@ if pgrep "ra32.miyoo" > /dev/null ; then
 	# } | $BIN_PATH/sendevent /dev/input/event3
 	# sleep 0.3
 	killall -q -15 ra32.miyoo
-elif pgrep "PPSSPPSDL" > /dev/null ; then
+elif pgrep "PPSSPPSDL" >/dev/null; then
 	{
-		echo 1 314 1  # SELECT down
-		echo 3 2 255  # L2 down
-		echo 3 2 0    # L2 up
-		echo 1 314 0  # SELECT up
-		echo 0 0 0    # tell sendevent to exit
+		echo 1 314 1 # SELECT down
+		echo 3 2 255 # L2 down
+		echo 3 2 0   # L2 up
+		echo 1 314 0 # SELECT up
+		echo 0 0 0   # tell sendevent to exit
 	} | $BIN_PATH/sendevent /dev/input/event4
 	sleep 1
 	killall -q -15 PPSSPPSDL
@@ -106,12 +88,12 @@ else
 	killall -q -9 MainUI
 fi
 
-# wait until emulator or MainUI exit 
-while killall -q -0 ra32.miyoo || \
-		killall -q -0 retroarch || \
-		killall -q -0 PPSSPPSDL || \
-		killall -q -0 drastic || \
-		killall -q -0 MainUI ; do 
+# wait until emulator or MainUI exit
+while killall -q -0 ra32.miyoo ||
+	killall -q -0 retroarch ||
+	killall -q -0 PPSSPPSDL ||
+	killall -q -0 drastic ||
+	killall -q -0 MainUI; do
 	sleep 0.5
 done
 
@@ -128,7 +110,7 @@ if flag_check "syncthing"; then
 		/mnt/SDCARD/App/Syncthing/syncthing_sync_check.sh --shutdown
 	fi
 
-    flag_remove "syncthing_startup_synced"
+	flag_remove "syncthing_startup_synced"
 fi
 
 # Saved current sound settings
