b129df2b3a9d9d78cdffc36c45e0f0ea39853939
cobaltgit <cobaltmail4204@gmail.com>
2024-10-23 16:19:20 +0100

    Add KMFD ParaLLeL Xtreme Amped core Look, no Vulkan!

--- Emu/.emu_setup/core/km_ludicrousn64_2k22_xtreme_amped.sh
+++ Emu/.emu_setup/core/km_ludicrousn64_2k22_xtreme_amped.sh
@@ -4,6 +4,6 @@ EMU_NAME="$(echo "$1" | cut -d'/' -f5)"
 CONFIG="/mnt/SDCARD/Emu/${EMU_NAME}/config.json"
 SYS_OPT="/mnt/SDCARD/Emu/.emu_setup/options/${EMU_NAME}.opt"
 
-sed -i 's|"Emu Core:ludicrousn64-(✓MUPEN64PLUS)"|"Emu Core: (✓LUDICROUSN64)-mupen64plus"|g' "$CONFIG"
-sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/km_ludicrousn64_2k22_xtreme_amped.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/mupen64plus.sh"|g' "$CONFIG"
+sed -i 's|"Emu Core:ludicrousn64-parallel-(✓MUPEN64PLUS)"|"Emu Core: (✓LUDICROUSN64)-parallel-mupen64plus"|g' "$CONFIG"
+sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/km_ludicrousn64_2k22_xtreme_amped.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/km_parallel_n64_xtreme_amped.sh"|g' "$CONFIG"
 sed -i 's|CORE=.*|CORE=\"km_ludicrousn64_2k22_xtreme_amped\"|g' "$SYS_OPT"
