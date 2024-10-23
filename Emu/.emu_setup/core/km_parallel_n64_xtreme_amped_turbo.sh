6f4e519a9dbbf5f4593e341224ad31b7ddeb30ff
cobaltgit <cobaltmail4204@gmail.com>
2024-10-23 16:44:02 +0100

    Turbo power!

--- Emu/.emu_setup/core/km_parallel_n64_xtreme_amped_turbo.sh
+++ Emu/.emu_setup/core/km_parallel_n64_xtreme_amped_turbo.sh
@@ -6,4 +6,4 @@ SYS_OPT="/mnt/SDCARD/Emu/.emu_setup/options/${EMU_NAME}.opt"
 
 sed -i 's|"Emu Core: (✓LUDICROUSN64)-parallel-mupen64plus"|"Emu Core: ludicrousn64-(✓PARALLEL)-mupen64plus"|g' "$CONFIG"
 sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/km_parallel_n64_xtreme_amped.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/mupen64plus.sh"|g' "$CONFIG"
-sed -i 's|CORE=.*|CORE=\"km_parallel_n64_xtreme_amped\"|g' "$SYS_OPT"
+sed -i 's|CORE=.*|CORE=\"km_parallel_n64_xtreme_amped_turbo\"|g' "$SYS_OPT"
