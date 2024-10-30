ea996a9d79f77fa4166b802f7f35deaca32541f2
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-10-29 21:31:47 -0500

    additional logging

--- Emu/.emu_setup/emu_setup.sh
+++ Emu/.emu_setup/emu_setup.sh
@@ -9,6 +9,8 @@ SPLORE_CART="/mnt/SDCARD/Roms/PICO8/-=☆ Launch Splore ☆=-.splore"
 # copy defaults folder into options folder if needed
 if [ ! -d "$OPT_DIR" ]; then
 	cp -rf "$DEF_DIR" "$OPT_DIR" && log_message "emu_setup.sh: copied $DEF_DIR into $OPT_DIR"
+else
+	log_message "emu_setup.sh: $OPT_DIR already exists"
 fi
 
 # copy standard RA launch scripts to all Emu subfolders
@@ -25,9 +27,13 @@ if [ ! -d "/mnt/SDCARD/.config" ]; then
 	else
 		log_message "emu_setup.sh: WARNING!!! No .config folder found!"
 	fi
+else
+	log_message "emu_setup.sh: .config folder already in place at SD card root."
 fi
 
 # create splore launcher if it doesn't already exist
 if [ ! -f "$SPLORE_CART" ]; then
 	touch "$SPLORE_CART" && log_message "emu_setup.sh: created $SPLORE_CART"
+else
+	log_message "emu_setup.sh: $SPLORE_CART already found."
 fi
