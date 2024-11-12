f4017bbd21af04a7ef2210efa5c48fcc2b54102b
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-11-12 15:50:24 -0600

    clean up help text slightly

--- spruce/scripts/applySetting/pico8_control_profiles.sh
+++ spruce/scripts/applySetting/pico8_control_profiles.sh
@@ -6,21 +6,21 @@
 
 # Steward
 if [ "$1" -eq 3 ]; then
-	echo -n "A->(o); B->(x); X->(pause); SELECT->(mouse)"
+	echo -n "A-(o) B-(x) X-(pause) SELECT-(mouse)"
 	return 0
 
 # Doubled/Face Buttons
 elif [ "$1" -eq 0 ]; then
-	echo -n "A->(o); B->(x); Y->(o); X->(x)"
+	echo -n "A-(o) B-(x) Y-(o) X-(x)"
 	return 0
 
 # One-handed
 elif [ "$1" -eq 2 ]; then
-	echo -n "A->(o); B->(x); L1->(o); L2->(x)"
+	echo -n "A-(o) B-(x) L1-(o) L2-(x)"
 	return 0
 
 # Racing/Default
 else ### if [ "$1" -eq 1 ]; then
-	echo -n "A->(o); B->(x); L1->(o); R1->(x)"
+	echo -n "A-(o) B-(x) L1-(o) R1-(x)"
 	return 0
 fi
