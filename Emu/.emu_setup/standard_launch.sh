a02967beb3a2fd26a7315c153c6381a270943532
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-11-15 21:09:24 -0600

    add advanced setting for stretching pico8 to fullscreen

--- Emu/.emu_setup/standard_launch.sh
+++ Emu/.emu_setup/standard_launch.sh
@@ -184,14 +184,20 @@ case $EMU_NAME in
 			cp -f "$P8_DIR/sdl_controllers.racing" "$P8_DIR/sdl_controllers.txt"
 		fi
 
+		if [ "$(setting_get "pico8_stretch")" = "0" ]; then
+			SCALING="-draw_rect 0,0,480,640"
+		else
+			SCALING=""
+		fi
+
 		export SDL_VIDEODRIVER=mali
 		export SDL_JOYSTICKDRIVER=a30
 		cd "$HOME"
 		sed -i 's|^transform_screen 0$|transform_screen 135|' "$HOME/.lexaloffle/pico-8/config.txt"
 		if [ "${GAME##*.}" = "splore" ]; then
-			pico8_dyn -splore -width 640 -height 480 -root_path "/mnt/SDCARD/Roms/PICO8/"
+			pico8_dyn -splore -width 640 -height 480 -root_path "/mnt/SDCARD/Roms/PICO8/" "$SCALING"
 		else
-			pico8_dyn -width 640 -height 480 -scancodes -run "$1"
+			pico8_dyn -width 640 -height 480 -scancodes -run "$1" "$SCALING"
 		fi
 		sync
 
