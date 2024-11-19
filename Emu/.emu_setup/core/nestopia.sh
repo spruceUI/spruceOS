8c00297f0b096ab2c824750020fbb1aa005ab93f
cobaltgit <cobaltmail4204@gmail.com>
2024-11-19 11:11:49 +0000

    Expose quicknes core to UI

--- Emu/.emu_setup/core/nestopia.sh
+++ Emu/.emu_setup/core/nestopia.sh
@@ -8,8 +8,14 @@ SYS_OPT="/mnt/SDCARD/Emu/.emu_setup/options/${EMU_NAME}.opt"
 
 display -i "$BG" -t "Core changed to nestopia"
 
-sed -i 's|"Emu Core: (✓FCEUMM)-nestopia"|"Emu Core: fceumm-(✓NESTOPIA)"|g' "$CONFIG"
-sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/nestopia.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/fceumm.sh"|g' "$CONFIG"
+if [ "$EMU_NAME" = "FC" ]; then
+    sed -i 's|"Emu Core: (✓FCEUMM)-nestopia-quicknes"|"Emu Core: fceumm-(✓NESTOPIA)-quicknes"|g' "$CONFIG"
+    sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/nestopia.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/quicknes.sh"|g' "$CONFIG"
+elif [ "$EMU_NAME" = "FDS" ]; then
+    sed -i 's|"Emu Core: (✓FCEUMM)-nestopia"|"Emu Core: fceumm-(✓NESTOPIA)"|g' "$CONFIG"
+    sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/nestopia.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/fceumm.sh"|g' "$CONFIG"
+fi
+
 sed -i 's|CORE=.*|CORE=\"nestopia\"|g' "$SYS_OPT"
 
 sleep 2
