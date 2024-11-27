5e9af93022b6726612690b21a7330f31bca75524
XanXic <XanderXic@gmail.com>
2024-11-26 22:34:25 -0500

    Sprucing up characters on Pico8 layout

--- spruce/scripts/applySetting/pico8_control_profiles.sh
+++ spruce/scripts/applySetting/pico8_control_profiles.sh
@@ -6,21 +6,21 @@
 
 # Steward
 if [ "$1" -eq 3 ]; then
-	echo -n "A-(x) B-(o) X-(esc) SELECT-(mouse)"
+	echo -n "A-ⓧ B-ⓞ X-Esc SELECT-Mouse"
 	return 0
 
 # Doubled/Face Buttons
 elif [ "$1" -eq 0 ]; then
-	echo -n "A-(x) B-(o) Y-(x) X-(o)"
+	echo -n "A-ⓧ B-ⓞ Y-ⓧ X-ⓞ"
 	return 0
 
 # One-handed
 elif [ "$1" -eq 2 ]; then
-	echo -n "A-(x) B-(o) L1-(x) L2-(o)"
+	echo -n "A-ⓧ B-ⓞ L1-ⓧ L2-ⓞ"
 	return 0
 
 # Racing/Default
 else ### if [ "$1" -eq 1 ]; then
-	echo -n "A-(x) B-(o) L1-(x) R1-(o)"
+	echo -n "A-ⓧ B-ⓞ L1-ⓧ R1-ⓞ"
 	return 0
 fi
