c490a43d515cac3156cd98f9c646fc0b297bee81
cobaltgit <cobaltmail4204@gmail.com>
2024-11-30 19:54:59 +0000

    Expose fmsx for MSX A very fast MSX emulator, approx. 2.5x faster than bluemsx

--- Emu/.emu_setup/core/bluemsx.sh
+++ Emu/.emu_setup/core/bluemsx.sh
@@ -8,8 +8,14 @@ SYS_OPT="/mnt/SDCARD/Emu/.emu_setup/options/${EMU_NAME}.opt"
 
 display -i "$BG" -t "Core changed to bluemsx"
 
-sed -i 's|"Emu Core: bluemsx-(✓GEARCOLECO)"|"Emu Core: (✓BLUEMSX)-gearcoleco"|g' "$CONFIG"
-sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/bluemsx.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/gearcoleco.sh"|g' "$CONFIG"
+if [ "$EMU_NAME" = "COLECO" ]; then
+    sed -i 's|"Emu Core: bluemsx-(✓GEARCOLECO)"|"Emu Core: (✓BLUEMSX)-gearcoleco"|g' "$CONFIG"
+    sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/bluemsx.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/gearcoleco.sh"|g' "$CONFIG"
+elif [ "$EMU_NAME" = "MSX" ]; then
+    sed -i 's|"Emu Core: bluemsx-(✓FMSX)"|"Emu Core: (✓BLUEMSX)-fmsx"|g' "$CONFIG"
+    sed -i 's|"/mnt/SDCARD/Emu/.emu_setup/core/fmsx.sh"|"/mnt/SDCARD/Emu/.emu_setup/core/fmsx.sh"|g' "$CONFIG"
+fi
+
 sed -i 's|CORE=.*|CORE=\"bluemsx\"|g' "$SYS_OPT"
 
 sleep 2
