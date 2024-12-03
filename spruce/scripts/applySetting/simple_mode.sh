6d81916cafe9bc02ae2e87be4a8eaea317dec623
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-12-02 20:36:17 -0600

    hide majority of apps when simple_mode.sh apply

--- spruce/scripts/applySetting/simple_mode.sh
+++ spruce/scripts/applySetting/simple_mode.sh
@@ -2,6 +2,14 @@
 
 ARGUMENT="$1"
 
+check_and_hide_app() {
+	SHOWHIDE="/mnt/SDCARD/spruce/scripts/applySetting/showHideApp.sh"
+	APP_DIR="/mnt/SDCARD/App"
+	APP="$1"
+	CONFIG="${APP_DIR}/${APP}/config.json"
+	[ "$("$SHOWHIDE" check "$CONFIG")" = "on" ] && "$SHOWHIDE" hide "$CONFIG"
+}
+
 if [ $ARGUMENT = "apply"]; then
 
 	# remove all X button menu items except aleatorio.sh
@@ -12,6 +20,19 @@ if [ $ARGUMENT = "apply"]; then
 			cp -f "${dir}/config.json.simple" "${dir}/config.json"
 		fi
 	done
+
+	# hide majority of apps
+	check_and_hide_app "-FirmwareUpdate-"
+	check_and_hide_app "-OTA"
+	check_and_hide_app "-Updater"
+	check_and_hide_app "AdvancedSettings"
+	check_and_hide_app "BootLogo"
+	check_and_hide_app "FileManagement"
+	check_and_hide_app "MiyooGamelist"
+	check_and_hide_app "RetroArch"
+	check_and_hide_app "ShowOutputTest"
+	check_and_hide_app "spruceBackup"
+	check_and_hide_app "ThemePacker"
 
 else # ARGUMENT is remove
 
