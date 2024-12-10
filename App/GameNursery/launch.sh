4ca6138655f6a0ab1793c0b6321283a2c74ccaa7
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-12-09 21:02:09 -0600

    initial test for generating nursery_config from freegame json files

--- App/GameNursery/launch.sh
+++ App/GameNursery/launch.sh
@@ -3,6 +3,7 @@
 BIN_PATH="/mnt/SDCARD/spruce/bin"
 NURSERY_DIR="/mnt/SDCARD/App/GameNursery"
 FLAGS_DIR="/mnt/SDCARD/spruce/flags"
+JSON_DIR="/mnt/SDCARD/Saves/nursery"
 
 # Copy of flag_check in helperFunctions.sh
 # Here to keep launch time short
@@ -15,6 +16,30 @@ flag_check() {
     fi
 }
 
+create_config_from_json() {
+
+    json_file="$1"
+    grouping="$(basename "$(dirname "$json_file")")"
+
+
+    display_name="$(jq -r '.display' "$json_file")"
+    file="$(jq -r '.file' "$json_file")"
+    system="$(jq -r '.system' "$json_file")"
+    url="$(jq -r '.url' "$json_file")"
+    description="$(jq -r '.description' "$json_file")"
+    requires_files="$(jq -r '.requires_files' "$json_file")"
+    version="$(jq -r '.version' "$json_file")"
+
+
+    if ! grep -q "\[$grouping\]" "$NURSERY_DIR"/nursery_config; then
+        echo "\[$grouping\]"
+    fi
+
+    echo "\"\" \"$display_name\" \"\|\" \"run\|off\" \"echo -n off\" \"\" \"\""
+    echo "\@$description"
+
+}
+
 # send signal USR2 to joystickinput to switch to KEYBOARD MODE
 # this allows joystick to be used as DPAD in setting app
 killall -q -USR2 joystickinput
@@ -22,28 +47,19 @@ killall -q -USR2 joystickinput
 # Initialize empty string for modes
 MODES=""
 
-# Add modes based on flag checks
-#if flag_check "developer_mode"; then
-#    MODES="$MODES -m Developer"
-#fi
-
-#if flag_check "designer_mode"; then
-#    MODES="$MODES -m Designer"
-#fi
-
-#PICO_DIR="/mnt/SDCARD/Emu/PICO8/bin/"
-#if [ -f "$PICO_DIR/pico8.dat" ] && [ -f "$PICO_DIR/pico8_dyn" ]; then
-#    MODES="$MODES -m Pico"
-#fi
-
-#if ! flag_check "simple_mode"; then
-#    MODES="$MODES -m Hide_if_simple"
-#fi
-
 # Easy to add more modes like this:
 # if flag_check "some_other_mode"; then
 #     MODES="$MODES -m other_mode"
 # fi
+
+echo "" > "$NURSERY_DIR"/nursery_config
+
+for filename in "$JSON_DIR"/*/*.json; do
+
+    create_config_from_json "$filename" >> "$NURSERY_DIR"/nursery_config
+
+done
+
 
 cd $BIN_PATH
 ./easyConfig "$NURSERY_DIR"/nursery_config $MODES
