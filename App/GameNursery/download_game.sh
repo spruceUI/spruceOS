97c4bd67005fe3901542767ee82f40a16075e6f3
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-12-11 18:54:10 -0600

    better logging, and make sure to delete 7z file after extraction

--- App/GameNursery/download_game.sh
+++ App/GameNursery/download_game.sh
@@ -10,6 +10,7 @@ ZIP_NAME="$(basename "$GAME_URL")"
 BG_IMG=/mnt/SDCARD/spruce/imgs/bg_tree.png
 
 # attempt to download the game
+log_message "Game Nursery: Attempting to download $GAME_NAME"
 display -i "$BG_IMG" -t "Now downloading $GAME_NAME!"
 if ! curl -s -k -L -o "$TMP_DIR/$ZIP_NAME" "$GAME_URL"; then
 	log_message "Game Nursery: Failed to download $GAME_NAME from $GAME_URL"
@@ -18,14 +19,16 @@ if ! curl -s -k -L -o "$TMP_DIR/$ZIP_NAME" "$GAME_URL"; then
 fi
 
 # attempt to unzip the game
+log_message "Game Nursery: Attempting to extract $GAME_NAME"
 display -i "$BG_IMG" -t "Now installing $GAME_NAME!"
 cd "/mnt/SDCARD"
 if ! 7zr x -y -scsUTF-8 "$TMP_DIR/$ZIP_NAME" >/dev/null 2>&1; then
 	display -d 3 --icon "/mnt/SDCARD/spruce/imgs/notfound.png" -t "Unable to extract latest game info files. Please try again later."
-	rm -f "$JSON_DIR/INFO.7z" >/dev/null 2>&1
-	log_message "Game Nursery: Failed to extract release info from INFO.7z file"
+	rm -f "$TMP_DIR/$ZIP_NAME" >/dev/null 2>&1
+	log_message "Game Nursery: Failed to extract $GAME_NAME from TMP_DIR/$ZIP_NAME"
 	exit 1
 else
 	display -d 2 -i "$BG_IMG" -t "$GAME_NAME installed successfully!"
-	log_message "Extraction process completed successfully"
+	log_message "Game Nursery: Extraction process completed successfully"
+	rm -f "$TMP_DIR/$ZIP_NAME" 2>/dev/null
 fi
