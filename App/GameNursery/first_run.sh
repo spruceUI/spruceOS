02bcc9d98521d7ecb7dde7fe61b1578b4cf6bd8f
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-12-13 15:37:14 -0600

    shorter version of intro text

--- App/GameNursery/first_run.sh
+++ App/GameNursery/first_run.sh
@@ -6,13 +6,11 @@ BG_IMG="/mnt/SDCARD/spruce/imgs/bg_tree.png"
 
 display --okay -i "$BG_IMG" -t "Welcome to the spruce Game Nursery! This is where we grow our curated collection of ports and free homebrew games for your enjoyment."
 
-display --okay -i "$BG_IMG" -t "Games hosted in the Nursery are organized into groups based on the system they were made for. Press L1 and R1 to tab through these groups."
+display --okay -i "$BG_IMG" -t "You can browse the games at your leisure, adding them to and removing them from your 'cart' as you go. Once you exit the app, your selections will be downloaded and installed in sequence."
 
-display --okay -i "$BG_IMG" -t "You can think of selecting games to download from the Nursery like a shopping cart; browse the games at your leisure, adding them to and removing them from your 'cart' as you go. Once you exit the app, your selections will all be downloaded and installed in sequence."
+display --okay -i "$BG_IMG" -t "Press L1 and R1 to tab through the different groups of games. The A button toggles a game in or out of your 'cart'. When you are ready to 'check out', press the B button."
 
-display --okay -i "$BG_IMG" -t "Games that you do not already have installed will display a brief description underneath them. If you highlight a game and press A, it will change to indicate that it will be installed."
-
-display --okay -i "$BG_IMG" -t "Games that you have already installed previously can also be easily reinstalled through this same interface, in the event that your previous installation has gone wrong."
+display --okay -i "$BG_IMG" -t "Games that you have previously installed can also be easily reinstalled through this same interface, if needed."
 
 display --okay -i "$BG_IMG" -t "The spruce team has the utmost respect for the developers of these games, and strives to comply with the respective license terms for each game in our collection."
 
