92529a6af2e3c4e58fb0e00321c9b379ed28e2ce
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-12-19 21:47:28 -0600

    Merge Brick's OpenBOR launch into standard_launch.sh

--- Emu/.emu_setup/standard_launch.sh
+++ Emu/.emu_setup/standard_launch.sh
@@ -156,13 +156,19 @@ case $EMU_NAME in
 		;;
 
 	"OPENBOR")
-		export LD_LIBRARY_PATH=lib:/usr/miyoo/lib:/usr/lib
-		export HOME=$EMU_DIR
-		cd $HOME
-		if [ "$GAME" == "Final Fight LNS.pak" ]; then
-			./OpenBOR_mod "$ROM_FILE"
-		else
-			./OpenBOR_new "$ROM_FILE"
+		if [ "$PLATFORM" = "Brick" ]; then
+			export HOME=$EMU_DIR
+			cd $HOME
+			./OpenBOR_Brick
+		else # assume A30
+			export LD_LIBRARY_PATH=lib:/usr/miyoo/lib:/usr/lib
+			export HOME=$EMU_DIR
+			cd $HOME
+			if [ "$GAME" == "Final Fight LNS.pak" ]; then
+				./OpenBOR_mod "$ROM_FILE"
+			else
+				./OpenBOR_new "$ROM_FILE"
+			fi
 		fi
 		sync
 		;;
