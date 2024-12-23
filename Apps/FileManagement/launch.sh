a5692d76b88b946e38b7fa3c47f8e3659114cdd5
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-12-22 20:25:44 -0600

    add A30's spruce theme icons at least as placeholder; prep File Management to get merged across platforms later

--- Apps/FileManagement/launch.sh
+++ Apps/FileManagement/launch.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd $(dirname "$0")
 
-export LD_LIBRARY_PATH=$(dirname "$0")/lib:$LD_LIBRARY_PATH
-./DinguxCommander
-
+export LD_LIBRARY_PATH=$(dirname "$0")/lib64:$LD_LIBRARY_PATH
+./DinguxCommanderBrick
