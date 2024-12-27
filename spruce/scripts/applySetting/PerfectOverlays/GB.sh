9806491198712cc3335b5792645d7eb1e0ffb432
XanXic <XanderXic@gmail.com>
2024-12-27 07:05:49 -0500

    Adding GBA to the apply/remove

--- spruce/scripts/applySetting/PerfectOverlays/GB.sh
+++ spruce/scripts/applySetting/PerfectOverlays/GB.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-. /mnt/SDCARD/spruce/scripts/helperFunctions.sh
+. /mnt/SDCARD/spruce/scripts/applySetting/PerfectOverlays/configEditHelpers.sh
 
 # File paths
 GB_GAMBATTE_CFG_FILE=/mnt/SDCARD/RetroArch/.retroarch/config/Gambatte/GB.cfg
@@ -10,48 +10,6 @@ GB_MGBA_CFG_FILE=/mnt/SDCARD/RetroArch/.retroarch/config/mGBA/GB.cfg
 GB_MGBA_GLSLP_FILE=/mnt/SDCARD/RetroArch/.retroarch/config/mGBA/GB.glslp
 GB_MGBA_GB_FILE=/mnt/SDCARD/RetroArch/.retroarch/config/mGBA/GB.opt
 
-# Helper function to update or create config file
-update_config_file() {
-    config_file="$1"
-    config_data="$2"
-    
-    # Create directory if it doesn't exist
-    mkdir -p "$(dirname "$config_file")"
-    
-    # Create file if it doesn't exist
-    [ ! -f "$config_file" ] && touch "$config_file"
-    
-    # Process each line of config data
-    echo "$config_data" | while IFS= read -r line; do
-        key="${line%%=*}"
-        key="$(echo "$key" | sed 's/[[:space:]]*$//')"  # Trim whitespace
-        
-        # Remove existing line with this key if it exists
-        sed -i "/${key}[[:space:]]*=/d" "$config_file"
-        
-        # Append new line
-        echo "$line" >> "$config_file"
-    done
-    
-    # Remove file if empty
-    [ ! -s "$config_file" ] && rm "$config_file"
-}
-
-# Helper function to remove config entries
-remove_config_entries() {
-    config_file="$1"
-    keys="$2"
-    
-    [ ! -f "$config_file" ] && return
-    
-    echo "$keys" | while IFS= read -r key; do
-        key="$(echo "$key" | sed 's/[[:space:]]*$//')"  # Trim whitespace
-        sed -i "/${key}[[:space:]]*=/d" "$config_file"
-    done
-    
-    # Remove file if empty
-    [ ! -s "$config_file" ] && rm "$config_file"
-}
 
 apply_overlay() {
     # Define configurations
@@ -150,7 +108,6 @@ mgba_gb_palette_subtle = \"Grand Ivory\""
     update_config_file "$GB_MGBA_GB_FILE" "$GB_MGBA_OPT_RESET"
 }
 
-log_message "Received command: $1"
 # Check for command line argument
 case "${1:-apply}" in
     "remove")
--- spruce/scripts/applySetting/PerfectOverlays/applyPerfectOs.sh
+++ spruce/scripts/applySetting/PerfectOverlays/applyPerfectOs.sh
@@ -1,28 +1,31 @@
-. /mnt/SDCARD/spruce/scripts/helperFunctions.sh
 
 . /mnt/SDCARD/spruce/scripts/applySetting/settingHelpers.sh
 
 # print minor info text with the value index zero (i.e. "on" value in config file )
 # this is placed before loading helping functions for fast checking
 if [ "$1" == "0" ]; then
-    echo -n "User: spruce, Password: happygaming, Port: 8080"
+    echo -n "Provided by Mugwomp93 and 1PlayerInsertCoin"
     return 0
 fi
 
 # print minor info text with the value index one (i.e. "off" value in config file )
 # this is placed before loading helping functions for fast checking
 if [ "$1" == "1" ]; then
-    echo -n "Manage your files wirelessly"
+    echo -n "Use "Perfect Overlays" for GB, GBC, and GBA consoles"
     return 0
 fi
+
+. /mnt/SDCARD/spruce/scripts/helperFunctions.sh
 
 if [ "$1" == "on" ]; then
     update_setting "perfect_overlays" "on"
     /mnt/SDCARD/spruce/scripts/applySetting/PerfectOverlays/GB.sh apply
+    /mnt/SDCARD/spruce/scripts/applySetting/PerfectOverlays/GBA.sh apply
     log_message "Turning on Perfect Overlays"
 
 elif [ "$1" == "off" ]; then
     update_setting "perfect_overlays" "off"
     /mnt/SDCARD/spruce/scripts/applySetting/PerfectOverlays/GB.sh remove
+    /mnt/SDCARD/spruce/scripts/applySetting/PerfectOverlays/GBA.sh remove
     log_message "Turning off Perfect Overlays"
 fi
