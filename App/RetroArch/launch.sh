2f4f49c70b30a903844112460f6d1752e39feefa
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2024-12-31 17:52:42 -0600

    Add Flip capability to RA app

--- App/RetroArch/launch.sh
+++ App/RetroArch/launch.sh
@@ -9,6 +9,8 @@ if [ "$PLATFORM" = "Brick" ]; then
 	HOME=$RA_DIR/ $RA_DIR/ra64.trimui -v
 elif [ "$PLATFORM" = "A30" ]; then
 	HOME=$RA_DIR/ $RA_DIR/ra32.miyoo -v
+elif [ "$PLATFORM" = "Flip" ]; then
+	HOME=$RA_DIR/ $RA_DIR/ra64.miyoo -v
 fi
 
 auto_regen_tmp_update
