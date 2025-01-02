760336de562d00b192b21be6f441de527da2c288
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2025-01-02 10:14:52 -0600

    Add compatibility for RTC app for Flip

--- App/RealTimeClock/launch.sh
+++ App/RealTimeClock/launch.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-./rtc
 . /mnt/SDCARD/spruce/scripts/helperFunctions.sh
+
+if [ "$PLATFORM" = "A30" ]; then
+	./rtc
+elif [ "$PLATFORM" = "Flip" ]; then
+	gptokeyb -k "rtc" -c "./rtc.gptk" &
+	sleep 1
+	./rtc-Flip
+	kill -9 $(pidof gptokeyb)
+fi
 auto_regen_tmp_update
