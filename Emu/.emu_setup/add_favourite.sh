bc8f67b5268e396170fd16068d6138687de75fcb
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2025-01-05 23:12:13 -0600

    fix favourite.sh bug; improve logging

--- Emu/.emu_setup/add_favourite.sh
+++ Emu/.emu_setup/add_favourite.sh
@@ -9,12 +9,12 @@ EMU_NAME="$(echo "$1" | cut -d'/' -f5)"
 ROMFILENAME="$(basename "$1")"
 GAMENAME="$(echo "$ROMFILENAME" | cut -d'.' -f1)"
 
-log_message "--- Adding favorite game chosen by user ---"
+log_message "Added $GAMENAME ($EMU_NAME) to favourite.json via X menu"
 
 # Construct the new JSON entry
 NEW_ENTRY=$(jq -n \
-  --arg label "$GAMENAME ($EMU_NAME)" \
-  --arg launch "/mnt/SDCARD/Emu/.emu_setup/standard_launch.sh" \
+  --arg label "$GAMENAME" \
+  --arg launch "/mnt/SDCARD/Emu/$EMU_NAME/../.emu_setup/standard_launch.sh" \
   --arg rompath "/mnt/SDCARD/Emu/$EMU_NAME/../../Roms/$EMU_NAME/$ROMFILENAME" \
   --argjson type "5" \
   '{label: $label, launch: $launch, rompath: $rompath, type: $type}')
