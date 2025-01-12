0775f183c9c76b78b39f96fbb7537563a4bd28ad
cobaltgit <cobaltmail4204@gmail.com>
2025-01-12 21:41:17 +0000

    Max stepping

--- spruce/scripts/helperFunctions.sh
+++ spruce/scripts/helperFunctions.sh
@@ -997,7 +997,7 @@ set_overclock() {
                 ;;
             "Brick"|"Flip")
                 echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-                echo 1992000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+                echo 2000000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
                 ;;
         esac
         chmod a-w /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
