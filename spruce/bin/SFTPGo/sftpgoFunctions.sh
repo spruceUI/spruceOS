c1fe19af6522ecc58abb8130ba68a02ea78e4142
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2025-01-18 23:28:26 -0600

    first steps to enable sftpgo on 64 bit devices

--- spruce/bin/SFTPGo/sftpgoFunctions.sh
+++ spruce/bin/SFTPGo/sftpgoFunctions.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 . /mnt/SDCARD/spruce/scripts/helperFunctions.sh
 
-SFTPGO_DIR="/mnt/SDCARD/spruce/bin/SFTPGo"
+if [ "$PLATFORM" = "A30" ]; then
+    SFTPGO_DIR="/mnt/SDCARD/spruce/bin/SFTPGo"
+else
+    SFTPGO_DIR="/mnt/SDCARD/spruce/bin64/SFTPGo"
+fi
 
 start_sftpgo_process() {
     log_message "Starting SFTPGO..."
-    nice -2 /mnt/SDCARD/spruce/bin/SFTPGo/sftpgo/sftpgo serve -c /mnt/SDCARD/spruce/bin/SFTPGo/sftpgo/ > /dev/null &
+    nice -2 $SFTPGO_DIR/sftpgo/sftpgo serve -c $SFTPGO_DIR/sftpgo/ > /dev/null &
     log_message "SFTPGO started with PID $!";
 }
 
