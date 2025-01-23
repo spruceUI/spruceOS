4a8a323ee06c3f3b64997c1ec44e1d651e46a523
Dan Hoizner <dan.hoizner@gmail.com>
2025-01-22 23:23:02 -0500

    chore(ssh): discriminate LD_LIBRARY_PATH based on platform

--- spruce/bin64/SSH/dropbear-wrapper.sh
+++ spruce/bin64/SSH/dropbear-wrapper.sh
@@ -3,6 +3,7 @@
 # Set the working directory to /mnt/SDCARD for all commands
 cd /mnt/SDCARD
 
+# TODO: this probably doesn't work outside the a30?
 if [ "$SSH_ORIGINAL_COMMAND" = "/usr/libexec/sftp-server" ]; then
   if [ -x "/mnt/SDCARD/miyoo/app/gesftpserver" ]; then
     exec /mnt/SDCARD/miyoo/app/gesftpserver
@@ -12,7 +13,12 @@ if [ "$SSH_ORIGINAL_COMMAND" = "/usr/libexec/sftp-server" ]; then
 elif [ -n "$SSH_ORIGINAL_COMMAND" ]; then
   exec $SSH_ORIGINAL_COMMAND
 else
-  export LD_LIBRARY_PATH=/usr/trimui/lib:
+  if [ "$PLATFORM" = "Brick" ]; then
+    export LD_LIBRARY_PATH="/usr/trimui/lib:$LD_LIBRARY_PATH"
+  elif [ "$PLATFORM" = "Flip" ]; then
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH"
+  fi
+  # TODO: what should this be for brick/flip?
   export PATH=/mnt/SDCARD/miyoo/app:/usr/sbin:/usr/bin:/sbin:/bin:/usr/sbin:/usr/bin:/sbin:/bin
   exec /bin/sh
 fi
