80a3c86fcfd146dc58c0f77feb09cf8c7a1a9327
cobaltgit <cobaltmail4204@gmail.com>
2025-02-04 16:53:11 +0000

    Swap genplusgxwide setting text

--- spruce/scripts/applySetting/genesisPlusGXWide.sh
+++ spruce/scripts/applySetting/genesisPlusGXWide.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-if [ "$1" = "0" ]; then
+if [ "$1" = "1" ]; then
     echo -n "Use Genesis Plus GX Wide core when available"
     return 0
-elif [ "$1" = "1" ]; then
+elif [ "$1" = "0" ]; then
     echo -n "NOTE: Will only apply to widescreen devices (TrimUI Smart Pro)"
     return 0
 fi
