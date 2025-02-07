9b4fddf59f2ad81a87dd6aa2d98cec1b61171a1d
cobaltgit <cobaltmail4204@gmail.com>
2025-02-07 19:02:40 +0000

    Scraper: add more system RA aliases, improve extlist logic

--- App/BoxartScraper/scraper.sh
+++ App/BoxartScraper/scraper.sh
@@ -17,6 +17,7 @@ get_ra_alias() {
     case $1 in
         AMIGA)               ra_name="Commodore - Amiga" ;;
         ATARI)               ra_name="Atari - 2600" ;;
+        ATARIST)             ra_name="Atari - ST" ;;
         ARCADE)              ra_name="MAME" ;;
         ARDUBOY)             ra_name="Arduboy Inc - Arduboy" ;;
         CHAI)                ra_name="ChaiLove" ;;
@@ -26,6 +27,7 @@ get_ra_alias() {
         DC)                  ra_name="Sega - Dreamcast" ;;
         DOOM)                ra_name="DOOM" ;;
         DOS)                 ra_name="DOS" ;;
+        EIGHTHUNDRED)        ra_name="Atari - 8-bit" ;;
         FAIRCHILD)           ra_name="Fairchild - Channel F" ;;
         FBNEO)               ra_name="FBNeo - Arcade Games" ;;
         FC)                  ra_name="Nintendo - Nintendo Entertainment System" ;;
@@ -35,6 +37,7 @@ get_ra_alias() {
         GBA)                 ra_name="Nintendo - Game Boy Advance" ;;
         GBC)                 ra_name="Nintendo - Game Boy Color" ;;
         GG)                  ra_name="Sega - Game Gear" ;;
+        GW)                  ra_name="Handheld Electronic Game" ;;
         INTELLIVISION)       ra_name="Mattel - Intellivision" ;;
         LYNX)                ra_name="Atari - Lynx" ;;
         MD)                  ra_name="Sega - Mega Drive - Genesis" ;;
@@ -79,75 +82,6 @@ get_ra_alias() {
         *) ra_name='' ;;
     esac
 }
-
-get_extensions() {
-    case $1 in
-        AMIGA)              extensions="adf adz dms fdi ipf hdf hdz lha slave info cue ccd nrg mds iso chd uae m3u zip 7z rp9" ;;
-        ARCADE)             extensions="zip" ;;
-        ARDUBOY)            extensions="hex" ;;
-        ATARI)              extensions="a26 bin zip 7z" ;;
-        CHAI)               extensions="chailove" ;;
-        COLECO)             extensions="rom ri mx1 mx2 col dsk cas sg sc m3u zip 7z" ;;
-        COMMODORE)          extensions="d64 zip 7z t64 crt prg nib tap" ;;
-        CPC)                extensions="sna dsk kcr bin zip 7z" ;;
-        CPS1|CPS2|CPS3)     extensions="zip 7z cue" ;;
-        DC)                 extensions="cdi gdi cue iso chd" ;;
-        DOOM)               extensions="zip wad exe" ;;
-        DOS)                extensions="zip dosz exe com bat iso ins img ima vhd jrc tc m3u m3u8 conf" ;;
-        EASYRPG)            extensions="zip ldb easyrpg" ;;
-        FAIRCHILD)          extensions="bin rom chf zip" ;;
-        FAKE08)             extensions="p8" ;;
-        FBNEO)              extensions="zip" ;;
-        FC|FDS)             extensions="fds nes unif unf zip 7z" ;;
-        FFPLAY)             extensions="mp4 mp3" ;;
-        FIFTYTWOHUNDRED)    extensions="a52 zip 7z bin" ;;
-        GB|GBC)             extensions="bin dmg gb gbc zip 7z" ;;
-        GBA)                extensions="bin gba zip 7z" ;;
-        GG)                 extensions="bin gg zip 7z" ;;
-        GW)                 extensions="mgw zip 7z" ;;
-        INTELLIVISION)      extensions="bin int zip 7z" ;;
-        LYNX)               extensions="lnx zip" ;;
-        MAME2003PLUS)       extensions="zip" ;;
-        MD|MS|MSUMD)        extensions="gen smd md 32x bin iso sms 68k chd zip 7z" ;;
-        MSU1)               extensions="sfc smc bml xml bs" ;;
-        MSX)                extensions="rom mx1 mx2 dsk cas zip 7z m3u" ;;
-        N64)                extensions="n64 v64 z64 bin usa pal jap zip 7z" ;;
-        NDS)                extensions="nds zip 7z rar" ;;
-        NEOCD)              extensions="cue chd m3u" ;;
-        NEOGEO)             extensions="zip 7z" ;;
-        NGP|NGPC)           extensions="ngp ngc zip 7z" ;;
-        OPENBOR)            extensions="pak" ;;
-        ODYSSEY)            extensions="bin zip 7z" ;;
-        PCE|PCECD)          extensions="pce ccd iso img chd cue zip 7z" ;;
-        PICO8)              extensions="p8 png p8.png" ;;
-        POKE)               extensions="min zip" ;;
-        PORTS)              extensions="zip sh" ;;
-        PS)                 extensions="bin cue img mdf pbp PBP toc cbn m3u chd" ;;
-        PSP)                extensions="iso cso" ;;
-        QUAKE)              extensions="fbl pak" ;;
-        SATELLAVIEW)        extensions="bs sfc smc swc fig st zip 7z" ;;
-        SCUMMVM)            extensions="scummvm" ;;
-        SEGACD|SEGASGONE)   extensions="gen smd md 32x cue iso sms 68k chd m3u zip 7z" ;;
-        SEVENTYEIGHTHUNDRED) extensions="a78 zip" ;;
-        SFC)                extensions="smc fig sfc gd3 gd7 dx2 bsx bs swc st zip 7z" ;;
-        SGB)                extensions="bin gb gbc gba zip 7z" ;;
-        SGFX)               extensions="pce sgx cue ccd chd zip 7z" ;;
-        SUFAMI)             extensions="smc zip 7z" ;;
-        SUPERVISION)        extensions="sv bin zip 7z" ;;
-        THIRTYTWOX)         extensions="gen smd md 32x bin iso sms 68k chd zip 7z" ;;
-        TIC)                extensions="tic fd sap k7 m7 rom zip 7z" ;;
-        VB)                 extensions="vb vboy zip 7z" ;;
-        VECTREX)            extensions="vec zip 7z" ;;
-        VIC20)              extensions="d64 d6z d71 d7z d80 d81 d82 d8z g64 g6z g41 g4z x64 x6z nib nbz d2m d4m t64 tap tcrt prg p00 crt bin cmd m3u vfl vsf zip 7z gz 20 40 60 a0 b0 rom" ;;
-        VIDEOPAC)           extensions="bin zip 7z" ;;
-        WOLF)               extensions="ecwolf exe" ;;
-        WS|WSC)             extensions="ws wsc pc2 zip 7z" ;;
-        X68000)             extensions="dim zip img d88 88d hdm dup 2hd xdf hdf cmd m3u 7z" ;;
-        ZXS)                extensions="tzx tap z80 rzx scl trd zip 7z" ;;
-        *)                  extensions='' ;;
-    esac
-}
-
 
 find_image_name() {
     local sys_name="$1"
@@ -238,7 +172,7 @@ if ! ping -c 2 thumbnails.libretro.com > /dev/null 2>&1; then
 fi
 
 # Set CPU governor to performance mode
-echo performance > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+set_performance
 
 # Process each system directory
 for sys_dir in "$roms_dir"/*/; do
@@ -256,8 +190,7 @@ for sys_dir in "$roms_dir"/*/; do
         continue
     fi
 
-    get_extensions "$sys_name"
-
+    extensions="$(jq -r '.extlist' "/mnt/SDCARD/Emu/$sys_name/config.json" | awk '{gsub(/\|/, " "); print $0}')"
     amount_games="$(find "$sys_dir" -type f -regex ".*\.\($(echo "$extensions" | sed 's/ /\\\|/g')\)$" | wc -l)"
     sys_label="$(jq ".label" "/mnt/SDCARD/Emu/$sys_name/config.json")"
     icon_path="$(jq ".iconsel" "/mnt/SDCARD/Emu/$sys_name/config.json")"
