cbf41b79e7a91e2e2bc6ad7409952cdaf28a24b4
cobaltgit <cobaltmail4204@gmail.com>
2025-02-09 16:51:48 +0000

    Fix overclock mode on trimui devices

--- spruce/scripts/helperFunctions.sh
+++ spruce/scripts/helperFunctions.sh
@@ -1037,7 +1037,7 @@ set_overclock() {
                 ;;
             "Brick"|"Flip"|"SmartPro")
                 echo performance >/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-                echo 1992000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+                echo 2000000 >/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
                 ;;
         esac
         chmod a-w /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
