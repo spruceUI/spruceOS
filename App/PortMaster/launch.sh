2ed94976e353c7b9d7cf2a11935ba43af0cc4709
Christopher Jacobs <chrisj951@gmail.com>
2025-04-04 19:55:53 -0700

    Start adding support for portmaster

---
+++ App/PortMaster/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export PYSDL2_DLL_PATH="/mnt/sdcard/MIYOO_EX/site-packages/sdl2dll/dll"
+export PATH="/mnt/sdcard/MIYOO_EX/bin/:$PATH"
+cd /sdcard/MIYOO_EX/PortMaster/PortMaster/miyoo
+./PortMaster.txt
