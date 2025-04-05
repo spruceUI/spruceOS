965782a3f5344310ee1d153334318be26961e59b
Christopher Jacobs <chrisj951@gmail.com>
2025-04-05 12:06:33 -0700

    Update libraries and add pkill for latest PortMaster

--- App/PortMaster/launch.sh
+++ App/PortMaster/launch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export PYSDL2_DLL_PATH="/mnt/sdcard/MIYOO_EX/site-packages/sdl2dll/dll"
 export PATH="/mnt/sdcard/MIYOO_EX/bin/:$PATH"
+export LD_LIBRARY_PATH="/mnt/sdcard/MIYOO_EX/lib/:$LD_LIBRARY_PATH"
 cd /sdcard/MIYOO_EX/PortMaster/PortMaster/miyoo
-./PortMaster.txt
+./PortMaster.txt > portmaster.log
