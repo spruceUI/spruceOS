834d100dba103879e4f93b89ecac51d693ec696e
Dan Hoizner <dan.hoizner@gmail.com>
2025-04-21 19:50:24 -0400

    fix(buttons): add a `setsharedmem` implementation targeted at the flip, volume and brightness persist across restarts

--- spruce/scripts/buttons_watchdog.sh
+++ spruce/scripts/buttons_watchdog.sh
@@ -4,6 +4,9 @@
 
 BIN_PATH="/mnt/SDCARD/spruce/bin64"
 [ "$PLATFORM" = "A30" ] && BIN_PATH="/mnt/SDCARD/spruce/bin"
+# TODO: maybe rename these to setsharedmem-$PLATFORM if we need brick/tsp ones?
+SETSHAREDMEM_PATH="$BIN_PATH/setsharedmem-flip"
+[ "$PLATFORM" = "A30" ] && SETSHAREDMEM_PATH="$BIN_PATH/setsharedmem"
 SET_OR_CSET="cset"
 [ "$PLATFORM" = "A30" ] && SET_OR_CSET="set"
 NAME_QUALIFIER="name="
@@ -132,7 +135,7 @@ brightness_down() {
 
         # write both level value to shared memory for MainUI to update its UI
         VOLUME_LV=$(get_volume_level)
-        $BIN_PATH/setsharedmem "$VOLUME_LV" "$BRIGHTNESS_LV"
+        $SETSHAREDMEM_PATH "$VOLUME_LV" "$BRIGHTNESS_LV"
     fi
 }
 
@@ -160,7 +163,7 @@ brightness_up() {
     
         # write both level value to shared memory for MainUI to update its UI
         VOLUME_LV=$(get_volume_level)
-        $BIN_PATH/setsharedmem "$VOLUME_LV" "$BRIGHTNESS_LV"
+        $SETSHAREDMEM_PATH "$VOLUME_LV" "$BRIGHTNESS_LV"
     fi
 }
 
@@ -200,7 +203,7 @@ volume_down() {
 
         # write both level value to shared memory for MainUI to update its UI
         BRIGHTNESS_LV=$(get_brightness_level)
-        $BIN_PATH/setsharedmem "$VOLUME_LV" "$BRIGHTNESS_LV"
+        $SETSHAREDMEM_PATH "$VOLUME_LV" "$BRIGHTNESS_LV"
     fi
 }
 
@@ -222,7 +225,7 @@ volume_up() {
 
         # write both level value to shared memory for MainUI to update its UI
         BRIGHTNESS_LV=$(get_brightness_level)
-        $BIN_PATH/setsharedmem "$VOLUME_LV" "$BRIGHTNESS_LV"
+        $SETSHAREDMEM_PATH "$VOLUME_LV" "$BRIGHTNESS_LV"
     fi
 }
 
