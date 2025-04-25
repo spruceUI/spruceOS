4a2166a2f37535e14dbac544a1ee1a338aa28fd2
Shreyan Mohanty <shreyanmohanty4@gmail.com>
2025-04-25 19:22:51 -0400

    Use gptokeyb on the flip

--- App/Credits/launch.sh
+++ App/Credits/launch.sh
@@ -9,10 +9,16 @@ if [ "$PLATFORM" = "A30" ]; then
     "/mnt/SDCARD/App/Credits/Sweater Ass Sounding Ass.mp3" \
     "/mnt/SDCARD/App/Credits/3 - Sir Daniel Bonaduce.mp3"
 else
+    [ "$PLATFORM" = "Flip" ] && /mnt/SDCARD/spruce/bin64/gptokeyb -k "as" -c "./as.gptk" & 
+    [ "$PLATFORM" = "Flip" ] && sleep 0.5
+
     /mnt/SDCARD/App/Credits/credits_app \
     /mnt/SDCARD/App/Credits/sprucepixelbg43.png \
     /mnt/SDCARD/App/Credits/credits.txt \
     /mnt/SDCARD/Themes/SPRUCE/nunwen.ttf 22 \
     "/mnt/SDCARD/App/Credits/Sweater Ass Sounding Ass.mp3" \
     "/mnt/SDCARD/App/Credits/3 - Sir Daniel Bonaduce.mp3"
+
+    [ "$PLATFORM" = "Flip" ] && kill -9 "$(pidpf gptokeyb)"
+
 fi
