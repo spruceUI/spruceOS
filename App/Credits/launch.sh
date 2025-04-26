9e4ae7037fa976c7b582623557367d87609f7b17
Shreyan Mohanty <shreyanmohanty4@gmail.com>
2025-04-26 05:36:29 +0530

    Fix syntax for calling credits app (#453)

--- App/Credits/launch.sh
+++ App/Credits/launch.sh
@@ -9,16 +9,15 @@ if [ "$PLATFORM" = "A30" ]; then
     "/mnt/SDCARD/App/Credits/Sweater Ass Sounding Ass.mp3" \
     "/mnt/SDCARD/App/Credits/3 - Sir Daniel Bonaduce.mp3"
 else
-    [ "$PLATFORM" = "Flip" ] && /mnt/SDCARD/spruce/bin64/gptokeyb -k "as" -c "./as.gptk" & 
+    [ "$PLATFORM" = "Flip" ] && /mnt/SDCARD/spruce/bin64/gptokeyb -k "credits_app" -c "./credits_app.gptk" & 
     [ "$PLATFORM" = "Flip" ] && sleep 0.5
 
     /mnt/SDCARD/App/Credits/credits_app \
     /mnt/SDCARD/App/Credits/sprucepixelbg43.png \
     /mnt/SDCARD/App/Credits/credits.txt \
-    /mnt/SDCARD/Themes/SPRUCE/nunwen.ttf 22 \
     "/mnt/SDCARD/App/Credits/Sweater Ass Sounding Ass.mp3" \
-    "/mnt/SDCARD/App/Credits/3 - Sir Daniel Bonaduce.mp3"
-
+    /mnt/SDCARD/Themes/SPRUCE/nunwen.ttf 22 
+    
     [ "$PLATFORM" = "Flip" ] && kill -9 "$(pidpf gptokeyb)"
 
 fi
