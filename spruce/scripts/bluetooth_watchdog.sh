2012ae0de7de79c0f84efd1445ade40ca76073a8
00svd00 <97850505+00svd00@users.noreply.github.com>
2025-05-01 07:07:45 +0300

    fix devices not connecting
    We also do no need to remove /tmp/bluetooth_ready
    It;s deleted automatically by  bluetoothd service

--- spruce/scripts/bluetooth_watchdog.sh
+++ spruce/scripts/bluetooth_watchdog.sh
@@ -7,8 +7,8 @@ while true; do
     inotifywait -e modify "$SYSTEM_JSON"
     BLUETOOTH="$(jq -r '.bluetooth' "$SYSTEM_JSON")"
     if [ $BLUETOOTH -eq 1 ]; then
+        /usr/bin/hciconfig hci0 up
+        /usr/bin/bluealsa -p a2dp-source &
         touch /tmp/bluetooth_ready
-    elif [ $BLUETOOTH -eq 0 ]; then
-        rm -f /tmp/bluetooth_ready
     fi
 done
