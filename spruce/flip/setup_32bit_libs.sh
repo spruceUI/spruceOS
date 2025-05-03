216854df468edfa907a2c933b5e3c98e96370e32
Christopher Jacobs <chrisj951@gmail.com>
2025-05-03 09:14:06 -0700

    Don't continue with mounting /usr if the overlay mount fails

--- spruce/flip/setup_32bit_libs.sh
+++ spruce/flip/setup_32bit_libs.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
+set -e  # Exit immediately if a command exits with a non-zero status
 
-mount_ext4() {
-	mount -o loop "/mnt/sdcard/Persistent/.32bit_merge/ext4.img" "/mnt/sdcard/Persistent/.32bit_merge/ext4"
+die() {
+	echo "Error: $1"
+	exit 1
 }
 
-bind_usr(){
-	mount -t overlay overlay -o lowerdir=/usr,upperdir=/mnt/sdcard/Persistent/.32bit_merge/ext4/usr/upper,workdir=/mnt/sdcard/Persistent/.32bit_merge/ext4/usr/work /mnt/sdcard/Persistent/.32bit_merge/ext4/usr/merged_usr
-	mount --bind "/mnt/sdcard/Persistent/.32bit_merge/ext4/usr/merged_usr" /usr
+mount_ext4() {
+	mount -o loop "/mnt/sdcard/Persistent/.32bit_merge/ext4.img" "/mnt/sdcard/Persistent/.32bit_merge/ext4" || die "Failed to mount ext4.img"
+}
+
+bind_usr() {
+	mount -t overlay overlay -o lowerdir=/usr,upperdir=/mnt/sdcard/Persistent/.32bit_merge/ext4/usr/upper,workdir=/mnt/sdcard/Persistent/.32bit_merge/ext4/usr/work /mnt/sdcard/Persistent/.32bit_merge/ext4/usr/merged_usr || die "Overlay mount failed"
+	mount --bind "/mnt/sdcard/Persistent/.32bit_merge/ext4/usr/merged_usr" /usr || die "Bind mount to /usr failed"
 }
 
 if [ ! -f "/mnt/sdcard/Persistent/.32bit_merge/ext4.img" ]; then
@@ -26,12 +32,10 @@ if [ ! -f "/mnt/sdcard/Persistent/.32bit_merge/ext4.img" ]; then
 	ln -s /mnt/sdcard/Persistent/.32bit_chroot/usr/lib32/ /usr/lib32
 	ln -s /mnt/sdcard/Persistent/.32bit_chroot/usr/lib32/ /usr/l32
 	ln -s /mnt/sdcard/Persistent/.32bit_chroot/usr/lib32/ /usr/arm-linux-gnueabihf
-	
+
 	cp /mnt/sdcard/spruce/flip/ld-linux-armhf.so.3 /usr/lib/
 
 else
 	mount_ext4
 	bind_usr
 fi
-
-
