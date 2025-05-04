a94c0cc6a9a99344c3f666f12cc110f5dd0751ea
Ryan Sartor <ryanmsartor@gmail.com>
2025-05-04 10:03:51 -0500

    Flip/Brick: Actually pass a rom file to OpenBOR when launching it

--- Emu/.emu_setup/standard_launch.sh
+++ Emu/.emu_setup/standard_launch.sh
@@ -188,10 +188,10 @@ run_openbor() {
 	export HOME=$EMU_DIR
 	cd $HOME
 	if [ "$PLATFORM" = "Brick" ]; then
-		./OpenBOR_Brick
+		./OpenBOR_Brick "$ROM_FILE"
 	elif [ "$PLATFORM" = "Flip" ]; then
 		export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME
-		./OpenBOR_Flip
+		./OpenBOR_Flip "$ROM_FILE"
 	else # assume A30
 		export LD_LIBRARY_PATH=lib:/usr/miyoo/lib:/usr/lib
 		if [ "$GAME" = "Final Fight LNS.pak" ]; then
