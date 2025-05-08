8469555f129c43693143f3e5e7af3bca21bb18a8
Christopher Jacobs <chrisj951@gmail.com>
2025-05-08 11:56:17 -0700

    Swap to the more MinUI like theme asset/config for PyUI

--- App/PyUI/launch.sh
+++ App/PyUI/launch.sh
@@ -2,8 +2,10 @@
 
 if [ -f /mnt/sdcard/App/PyUI/.enabled ]; then
     rm /mnt/SDCARD/App/PyUI/.enabled
+    cp -R /mnt/SDCARD/App/PyUI/theme-overrides/MainUI/Themes/* /mnt/SDCARD/Themes/
 else
     echo "" > "/mnt/SDCARD/App/PyUI/.enabled"
+    cp -R /mnt/SDCARD/App/PyUI/theme-overrides/PyUI/Themes/* /mnt/SDCARD/Themes/
 fi
 
 reboot
