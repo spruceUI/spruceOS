5283de96981b793022ba8d588b60d6cd86c0cb81
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2025-05-10 19:29:17 -0500

    BP: guilt trip controls execution loop

--- App/BitPal/bitpal.sh
+++ App/BitPal/bitpal.sh
@@ -3,9 +3,11 @@
 . /mnt/SDCARD/spruce/scripts/helperFunctions.sh
 . /mnt/SDCARD/App/BitPal/BitPalFunctions.sh
 
-[ ! -f $BITPAL_JSON ] && initialize_bitpal_data
-[ ! -f "$MISSION_JSON" ] && initialize_mission_data
+export PYSDL2_DLL_PATH="/mnt/SDCARD/App/PyUI/dll"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/miyoo/lib"
 
+[ ! -f "$BITPAL_JSON" ] && initialize_bitpal_data
+[ ! -f "$MISSION_JSON" ] && initialize_mission_data
 
 set_random_good_mood ##### just for testing
 FACE="$(get_face)"
@@ -20,19 +22,25 @@ FACT="$(get_random_fact)"
 display --okay -t "$FACT" -s 36 -p 50
 sleep 0.1
 
-# Launch main BitPal menu
-export PYSDL2_DLL_PATH="/mnt/SDCARD/App/PyUI/dll"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/miyoo/lib"
-call_menu "BitPal - Main" "main.json"
+leave=false
+while [ ! "$leave" = true ]; do
+    # Launch main BitPal menu
+    call_menu "BitPal - Main" "main.json"
 
-set_random_negative_mood ##### just for testing
-case "$mood" in
-    sad|angry|neutral|surprised)
-        GUILT_TRIP="$(get_random_guilt_trip)"
-        display --okay -t "$GUILT_TRIP" -s 36 -p 50
-        sleep 0.1
-        ;;
-    *) 
-        true
-        ;;
-esac
+    set_random_negative_mood ##### just for testing
+    mood="$(get_bitpal_mood)"
+    case "$mood" in
+        sad|angry|neutral|surprised)
+            GUILT_TRIP="$(get_random_guilt_trip)"
+            display --confirm -t "$GUILT_TRIP" -s 36 -p 50
+            if confirm; then
+                leave=true
+            else
+                leave=false
+            fi
+            ;;
+        *) 
+            true
+            ;;
+    esac
+done
