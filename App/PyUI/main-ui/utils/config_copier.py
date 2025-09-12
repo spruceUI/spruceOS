cea2a1eaf2024dace2114d40776b7cb42592d592
Christopher Jacobs <chrisj951@gmail.com>
2025-09-12 13:51:43 -0500

    Update PyUI to 495955c0c67a13b2e2b53ac404c347d35170f15d
    Ensure config file is greater than size 0

--- App/PyUI/main-ui/utils/config_copier.py
+++ App/PyUI/main-ui/utils/config_copier.py
@@ -3,6 +3,8 @@
 from pathlib import Path
 import shutil
 
+from utils.logger import PyUiLogger
+
 
 class ConfigCopier:
 
@@ -13,15 +15,18 @@ class ConfigCopier:
         If it does not, copies 'config.json' from the script's directory to that location.
         """
         dest = Path(file_path)
-        if dest.exists():
+        if dest.exists() and dest.stat().st_size > 0:
+            PyUiLogger.get_logger().error(f"{file_path} already exists")
             return  # Nothing to do
 
         if not default_config_file.exists():
+            PyUiLogger.get_logger().error(f"{default_config_file} does not exist")
             raise FileNotFoundError(f"Source config file not found: {default_config_file}")
 
         # Create parent directories if necessary
         dest.parent.mkdir(parents=True, exist_ok=True)
 
+        PyUiLogger.get_logger().info(f"Copying {default_config_file} to {file_path}")
         # Copy the file
         shutil.copy2(default_config_file, dest)
         
