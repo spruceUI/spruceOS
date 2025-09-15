93b5834d631f3b79900dfcf91aef9afbd5448e60
Christopher Jacobs <chrisj951@gmail.com>
2025-09-14 20:34:53 -0500

    Update PyUI to 1043c50a2848a503dd45984498ef1386c3a04de6
    Miyoogameslist doesn't have extensions
    Account for mapped names for recents/favorites
    Do not remove ext from folder names. Folders do not have extensions
    Fix single line miyoogameslist.xmls

--- App/PyUI/main-ui/menus/games/utils/rom_info.py
+++ App/PyUI/main-ui/menus/games/utils/rom_info.py
@@ -10,9 +10,11 @@ class RomInfo:
     game_system: GameSystem
     rom_file_path: str
     is_collection: bool
+    display_name: str
 
-    def __init__(self, game_system: GameSystem, rom_file_path: str, is_collection=False):
+    def __init__(self, game_system: GameSystem, rom_file_path: str, display_name=None, is_collection=False):
         self.game_system = game_system
         self.rom_file_path = rom_file_path
+        self.display_name = display_name
         self.is_collection = is_collection
         
