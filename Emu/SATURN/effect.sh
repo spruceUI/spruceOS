1c6b76bcb13958254e1032e1b124a9e31096c2d4
Ryan Sartor <ryanmsartor@gmail.com>
2025-10-06 21:22:40 -0500

    remove effect.sh for all emus; these can be offloaded to pyui eventually

--- Emu/SATURN/effect.sh
+++
@@ -1,5 +0,0 @@
-echo 1 > /sys/class/led_anim/effect_enable 
-echo E4007F > /sys/class/led_anim/effect_rgb_hex_lr
-echo 1 > /sys/class/led_anim/effect_cycles_lr
-echo 1000 > /sys/class/led_anim/effect_duration_lr
-echo 1 >  /sys/class/led_anim/effect_lr
--- Emu/WSC/effect.sh
+++
@@ -1,5 +0,0 @@
-echo 1 > /sys/class/led_anim/effect_enable 
-echo 3C4593 > /sys/class/led_anim/effect_rgb_hex_lr
-echo 1 > /sys/class/led_anim/effect_cycles_lr
-echo 1000 > /sys/class/led_anim/effect_duration_lr
-echo 1 >  /sys/class/led_anim/effect_lr
