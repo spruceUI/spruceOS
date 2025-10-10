bf737cceb18fd80baebc4ab9c9e99af2aa681e36
Christopher Jacobs <chrisj951@gmail.com>
2025-10-09 19:57:51 -0500

    Search subfolders as well when searching

--- App/PyUI/main-ui/menus/games/search_games_for_system_menu.py
+++ App/PyUI/main-ui/menus/games/search_games_for_system_menu.py
@@ -16,11 +16,43 @@ class SearchGamesForSystemMenu(RomsMenuCommon):
         self.game_system = game_system
         self.search_str = search_str
 
-    def include_rom(self,rom_file_name):
-        return self.search_str in rom_file_name.upper()
+    def include_rom(self,rom_file_name, rom_file_path):
+        return self.search_str in rom_file_name.upper() or os.path.isdir(rom_file_path)
     
     def _get_rom_list(self) -> list[GridOrListEntry]:
-        return self.rom_select_options_builder.build_rom_list(self.game_system, self.include_rom)
+        roms = []
+
+        def _collect_roms_recursively(game_system, directory=None):
+
+            """Recursively collect roms for a given game_system and directory."""
+            rom_list = self.rom_select_options_builder.build_rom_list(
+                game_system,
+                self.include_rom,
+                subfolder=directory
+            )
+            all_roms = []
+
+            # Iterate through entries to find directories
+            for entry in rom_list:
+                rom_path = entry.get_value().rom_file_path
+                rom_name = os.path.basename(rom_path)
+                if os.path.isdir(rom_path):
+                    # Recurse into subdirectory
+                    sub_roms = _collect_roms_recursively(game_system, rom_path)
+                    all_roms.extend(sub_roms)
+                    if(self.search_str in rom_name.upper()):
+                        all_roms.append(entry)
+                else:
+                    all_roms.append(entry)
+
+            return all_roms
+
+        # Iterate all game systems
+        roms_for_system = _collect_roms_recursively(self.game_system)
+        roms.extend(roms_for_system)
+
+        return roms
+
 
     def run_rom_selection(self) :
         return self._run_rom_selection(f"{self.game_system.display_name} Search")
--- App/PyUI/main-ui/menus/games/searched_roms_menu.py
+++ App/PyUI/main-ui/menus/games/searched_roms_menu.py
@@ -5,6 +5,7 @@ from devices.device import Device
 from menus.games.roms_menu_common import RomsMenuCommon
 from menus.games.utils.rom_info import RomInfo
 from menus.games.utils.rom_select_options_builder import RomSelectOptionsBuilder
+from utils.logger import PyUiLogger
 from views.grid_or_list_entry import GridOrListEntry
 
 
@@ -14,16 +15,43 @@ class SearchedRomsMenu(RomsMenuCommon):
         self.rom_select_options_builder = RomSelectOptionsBuilder()
         self.search_str = search_str
 
-    def include_rom(self,rom_file_name):
-        return self.search_str in rom_file_name.upper()
-    
+    def include_rom(self,rom_file_name, rom_file_path):
+        return self.search_str in rom_file_name.upper() or os.path.isdir(rom_file_path)
+        
     def _get_rom_list(self) -> list[GridOrListEntry]:
         roms = []
         game_utils = Device.get_game_system_utils()
-        for game_system in game_utils.get_active_systems():
-            roms += self.rom_select_options_builder.build_rom_list(game_system, self.include_rom)
-        
-        return roms
 
+        def _collect_roms_recursively(game_system, directory=None):
+
+            """Recursively collect roms for a given game_system and directory."""
+            rom_list = self.rom_select_options_builder.build_rom_list(
+                game_system,
+                self.include_rom,
+                subfolder=directory
+            )
+            all_roms = []
+
+            # Iterate through entries to find directories
+            for entry in rom_list:
+                rom_path = entry.get_value().rom_file_path
+                rom_name = os.path.basename(rom_path)
+                if os.path.isdir(rom_path):
+                    # Recurse into subdirectory
+                    sub_roms = _collect_roms_recursively(game_system, rom_path)
+                    all_roms.extend(sub_roms)
+                    if(self.search_str in rom_name.upper()):
+                        all_roms.append(entry)
+                else:
+                    all_roms.append(entry)
+
+            return all_roms
+
+        # Iterate all game systems
+        for game_system in game_utils.get_active_systems():
+            roms_for_system = _collect_roms_recursively(game_system)
+            roms.extend(roms_for_system)
+
+        return roms
     def run_rom_selection(self) :
         return self._run_rom_selection("Game Search")
