5172afbda11c7d04c12846919b7d4a5a15181f53
Ryan Sartor <ryanmsartor@gmail.com>
2025-10-27 21:48:05 -0500

    Advanced Settings: move platform cheack ahead of BIN_PATH setting; minor cleanup

--- App/AdvancedSettings/launch.sh
+++ App/AdvancedSettings/launch.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 
+# Copy of platform detection from helperFunctions.sh
+INFO=$(cat /proc/cpuinfo 2> /dev/null)
+case $INFO in
+    *"sun8i"*)  export PLATFORM="A30" ;;
+    *"TG5040"*) export PLATFORM="SmartPro" ;;
+    *"TG3040"*) export PLATFORM="Brick" ;;
+    *"0xd05"*)  export PLATFORM="Flip" ;;
+    *)          export PLATFORM="A30" ;;
+esac
+
 if [ "$PLATFORM" = "A30" ]; then
-  BIN_PATH="/mnt/SDCARD/spruce/bin"
+    BIN_PATH="/mnt/SDCARD/spruce/bin"
 else
-  BIN_PATH="/mnt/SDCARD/spruce/bin64"
+    BIN_PATH="/mnt/SDCARD/spruce/bin64"
 fi
+
 SETTINGS_PATH="/mnt/SDCARD/spruce/settings"
 FLAGS_DIR="/mnt/SDCARD/spruce/flags"
-
 HELPER_PATH="/mnt/SDCARD/spruce/scripts/applySetting/settingHelpers.sh"
 
-# Copy of flag_check in helperFunctions.sh
-# Here to keep launch time short
+# Copy of just flag_check from helperFunctions.sh here to keep launch time short
 flag_check() {
     local flag_name="$1"
     if [ -f "$FLAGS_DIR/${flag_name}" ] || [ -f "$FLAGS_DIR/${flag_name}.lock" ]; then
@@ -20,28 +29,6 @@ flag_check() {
         return 1
     fi
 }
-
-# Copy of platform detection from helperFunctions.sh
-# Detect device and export to any script sourcing helperFunctions
-INFO=$(cat /proc/cpuinfo 2> /dev/null)
-
-case $INFO in
-*"sun8i"*)
-	export PLATFORM="A30"
-    ;;
-*"TG5040"*)
-	export PLATFORM="SmartPro"
-	;;
-*"TG3040"*)
-	export PLATFORM="Brick"
-	;;
-*"0xd05"*)
-    export PLATFORM="Flip"
-    ;;
-*)
-    export PLATFORM="A30"
-    ;;
-esac
 
 # send signal USR2 to joystickinput to switch to KEYBOARD MODE
 # this allows joystick to be used as DPAD in setting app
@@ -70,12 +57,9 @@ fi
 # Add a mode based on which device spruce is running on
 MODES="$MODES -m $PLATFORM"
 
-if [ -f "/mnt/SDCARD/.DS_Store" ]; then MODES="$MODES -m Mac"; fi # will mac always create a junk file at the sdcard root?
-
-# Easy to add more modes like this:
-# if flag_check "some_other_mode"; then
-#     MODES="$MODES -m other_mode"
-# fi
+if [ -f "/mnt/SDCARD/.DS_Store" ]; then
+    MODES="$MODES -m Mac"
+fi
 
 [ "$PLATFORM" = "Flip" ] && echo -1 > /sys/class/miyooio_chr_dev/joy_type
 
@@ -84,10 +68,10 @@ if [ ! "$PLATFORM" = "A30" ]; then
 	sleep 0.5
 fi
 
-cd $BIN_PATH
-./easyConfig $SETTINGS_PATH/settings_config $MODES
+cd "$BIN_PATH"
+./easyConfig "$SETTINGS_PATH/settings_config" $MODES
 
-kill -9 "$(pidof gptokeyb)"
+kill -9 "$(pidof gptokeyb)" 2>/dev/null
 
 # send signal USR1 to joystickinput to switch to ANALOG MODE
 killall -q -USR1 joystickinput
