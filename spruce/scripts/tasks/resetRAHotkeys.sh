a256bcf8d133a67e91350acaa11b68da6478860d
Ryan Sartor <ryanmsartor@gmail.com>
2025-11-17 21:53:56 -0600

    wire up resetRAHotkeys task for more than just A30

--- spruce/scripts/tasks/resetRAHotkeys.sh
+++ spruce/scripts/tasks/resetRAHotkeys.sh
@@ -22,25 +22,39 @@ update_file() {
 log_message "Resetting RetroArch hotkeys to Spruce defaults."
 
 # Update RetroArch config with default values
-update_file "$RA_FILE" \
-    "input_enable_hotkey = \"rctrl\"" \
-    "input_exit_emulator = \"ctrl\"" \
-    "input_fps_toggle = \"alt\"" \
-    "input_joypad_driver = \"linuxraw\"" \
-    "input_load_state = \"tab\"" \
-    "input_menu_toggle = \"shift\"" \
-    "input_menu_toggle_btn = \"9\"" \
-    "input_player1_l_x_minus_axis = \"-0\"" \
-    "input_player1_l_x_plus_axis = \"+0\"" \
-    "input_player1_l_y_minus_axis = \"-1\"" \
-    "input_player1_l_y_plus_axis = \"+1\"" \
-    "input_quit_gamepad_combo = \"0\"" \
-    "input_save_state = \"backspace\"" \
-    "input_screenshot = \"space\"" \
-    "input_shader_toggle = \"up\"" \
-    "input_state_slot_decrease = \"left\"" \
-    "input_state_slot_increase = \"right\"" \
-    "input_toggle_slowmotion = \"e\"" \
-    "input_toggle_fast_forward = \"t\""
+
+if [ "$PLATFORM" = "A30" ]; then
+    update_file "$RA_FILE" \
+        "input_enable_hotkey = \"rctrl\"" \
+        "input_exit_emulator = \"ctrl\"" \
+        "input_fps_toggle = \"alt\"" \
+        "input_load_state = \"tab\"" \
+        "input_menu_toggle = \"shift\"" \
+        "input_menu_toggle_btn = \"9\"" \
+        "input_quit_gamepad_combo = \"0\"" \
+        "input_save_state = \"backspace\"" \
+        "input_screenshot = \"space\"" \
+        "input_shader_toggle = \"up\"" \
+        "input_state_slot_decrease = \"left\"" \
+        "input_state_slot_increase = \"right\"" \
+        "input_toggle_slowmotion = \"e\"" \
+        "input_toggle_fast_forward = \"t\""
+else
+    update_file "$RA_FILE" \
+        "input_enable_hotkey_btn = \"4\"" \
+        "input_exit_emulator_btn = \"0\"" \
+        "input_fps_toggle_btn = \"2\"" \
+        "input_load_state_btn = \"9\"" \
+        "input_menu_toggle = \"escape\"" \
+        "input_menu_toggle_btn = \"3\"" \
+        "input_quit_gamepad_combo = \"0\"" \
+        "input_save_state_btn = \"10\"" \
+        "input_screenshot_btn = \"1\"" \
+        "input_shader_toggle_btn = \"11\"" \
+        "input_state_slot_decrease_btn = \"13\"" \
+        "input_state_slot_increase_btn = \"14\"" \
+        "input_toggle_slowmotion_axis = \"+4\"" \
+        "input_toggle_fast_forward_axis = \"+5\""
+fi
 
 log_message "RetroArch hotkeys have been reset to defaults."
