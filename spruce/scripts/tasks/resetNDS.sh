522c0dd535419882a95fcf952b666f05b8a8b9cd
Ryan Sartor <ryanmsartor@gmail.com>
2025-11-20 17:50:46 -0600

    Add task to reset NDS configs

---
+++ spruce/scripts/tasks/resetNDS.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+. /mnt/SDCARD/spruce/scripts/helperFunctions.sh
+
+ORIGINAL_NDS_FILE="/mnt/SDCARD/Emu/NDS/drastic-$PLATFORM.cfg"
+BACKUP_NDS_FILE="/mnt/SDCARD/Emu/NDS/drastic-$PLATFORM.cfg.bak"
+
+ORIGINAL_CF2_FILE="/mnt/SDCARD/Emu/NDS/drastic.cf2"
+BACKUP_CF2_FILE="/mnt/SDCARD/Emu/NDS/drastic.cf2.bak"
+
+log_message "Resetting DraStic config to default."
+cp -f "$BACKUP_NDS_FILE" "$ORIGINAL_NDS_FILE"
+cp -f "$BACKUP_CF2_FILE" "$ORIGINAL_CF2_FILE"
