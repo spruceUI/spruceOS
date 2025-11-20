f9a4680015993854cc5a6f4fb6f519d7e64766e9
Ryan Sartor <ryanmsartor@gmail.com>
2025-11-20 17:57:53 -0600

    correct file paths

--- spruce/scripts/tasks/resetNDS.sh
+++ spruce/scripts/tasks/resetNDS.sh
@@ -2,11 +2,11 @@
 
 . /mnt/SDCARD/spruce/scripts/helperFunctions.sh
 
-ORIGINAL_NDS_FILE="/mnt/SDCARD/Emu/NDS/drastic-$PLATFORM.cfg"
-BACKUP_NDS_FILE="/mnt/SDCARD/Emu/NDS/drastic-$PLATFORM.cfg.bak"
+ORIGINAL_NDS_FILE="/mnt/SDCARD/Emu/NDS/config/drastic-$PLATFORM.cfg"
+BACKUP_NDS_FILE="/mnt/SDCARD/Emu/NDS/config/drastic-$PLATFORM.cfg.bak"
 
-ORIGINAL_CF2_FILE="/mnt/SDCARD/Emu/NDS/drastic.cf2"
-BACKUP_CF2_FILE="/mnt/SDCARD/Emu/NDS/drastic.cf2.bak"
+ORIGINAL_CF2_FILE="/mnt/SDCARD/Emu/NDS/config/drastic.cf2"
+BACKUP_CF2_FILE="/mnt/SDCARD/Emu/NDS/config/drastic.cf2.bak"
 
 log_message "Resetting DraStic config to default."
 cp -f "$BACKUP_NDS_FILE" "$ORIGINAL_NDS_FILE"
