b278332b1c0e90dfe22ac19379552b86e82ef4fc
Ryan Sartor <ryanmsartor@gmail.com>
2025-11-19 19:56:10 -0600

    partial fix for credits. keeping hidden for now.

--- App/Credits/launch.sh
+++ App/Credits/launch.sh
@@ -4,15 +4,31 @@ HERE="$(dirname "$0")"
 cd "$HERE"
 
 if [ "$PLATFORM" = "A30" ]; then
+
     "$HERE"/credits_app.elf \
     "$HERE"/sprucepixelbg43.png \
     "$HERE"/credits.txt \
     /mnt/SDCARD/Themes/SPRUCE/nunwen.ttf 22 \
     "$HERE"/Sweater Ass Sounding Ass.mp3" \
     "$HERE"/3 - Sir Daniel Bonaduce.mp3"
-else
-    [ "$PLATFORM" = "Flip" ] && /mnt/SDCARD/spruce/bin64/gptokeyb -k "credits_app" -c "./credits_app.gptk" & 
-    [ "$PLATFORM" = "Flip" ] && sleep 0.5
+
+elif [ "$PLATFORM" = "Flip" ]; then
+    /mnt/SDCARD/spruce/bin64/gptokeyb -k "credits_app" -c "./credits_app.gptk" & 
+    sleep 0.5
+
+    export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/miyoo/lib
+
+    "$HERE"/credits_app \
+    "$HERE"/sprucepixelbg43.png \
+    "$HERE"/credits.txt \
+    /mnt/SDCARD/Themes/SPRUCE/nunwen.ttf 22 \
+    "$HERE"/Sweater Ass Sounding Ass.mp3" \
+    "$HERE"/3 - Sir Daniel Bonaduce.mp3" 
+    kill -9 "$(pidof gptokeyb)"
+
+else    # trimui devices - currently not working due to needing a newer glibc
+
+    export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/trimui/lib
 
     "$HERE"/credits_app \
     "$HERE"/sprucepixelbg43.png \
@@ -21,5 +37,4 @@ else
     "$HERE"/Sweater Ass Sounding Ass.mp3" \
     "$HERE"/3 - Sir Daniel Bonaduce.mp3" 
 
-    [ "$PLATFORM" = "Flip" ] && kill -9 "$(pidof gptokeyb)"
 fi
