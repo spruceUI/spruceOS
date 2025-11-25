18ab474c2551d833161d1c78359df0a962678502
SundownerSport <kilchisblue@aol.com>
2025-11-24 17:27:50 -0800

    clean up game nursery wording
    sanitize of "cart" references

--- App/GameNursery/first_run.sh
+++ App/GameNursery/first_run.sh
@@ -5,9 +5,7 @@
 
 display --okay -i "$BG_IMG" -t "Welcome to the spruce Game Nursery! This is where we grow our curated collection of ports and free homebrew games for your enjoyment."
 sleep 0.05
-display --okay -i "$BG_IMG" -t "You can browse the games at your leisure, adding them to and removing them from your 'cart' as you go. Once you exit the app, your selections will be downloaded and installed in sequence."
-sleep 0.05
-display --okay -i "$BG_IMG" -t "Press L1 and R1 to tab through the different groups of games. The A button toggles a game in or out of your 'cart'. When you are ready to 'check out', press the B button."
+display --okay -i "$BG_IMG" -t "You can browse the games at your leisure, selections will be downloaded and installed automaticially."
 sleep 0.05
 display --okay -i "$BG_IMG" -t "Games that you have previously installed can also be easily reinstalled through this same interface, if needed."
 sleep 0.05
