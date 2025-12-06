8c7c380761e36b3ad9f6285d2ff357e142b41fc9
cobaltgit <cobaltsecuremail312@proton.me>
2025-12-06 16:35:03 +0000

    fixy fixy

--- spruce/brick/sdl2/bind.sh
+++ spruce/brick/sdl2/bind.sh
@@ -50,17 +50,18 @@ case "$PLATFORM" in
 		/usr/lib/libSDL2_ttf-2.0.so.0 \
 		/usr/lib/libSDL2_ttf-2.0.so.0.18.1 \
 		/usr/lib/libSDL_mixer-1.2.so.0 \
-		/usr/lib/libSDL_mixer-1.2.so.0.8.5 \
+		/usr/lib/libSDL_mixer-1.2.so.0.12.1 \
 		/usr/lib/libSDL_ttf-2.0.so.0 \
 		/usr/lib/libSDL_ttf-2.0.so.0.18.1 \
 		/usr/trimui/lib/libgamename.so \
 		/usr/trimui/lib/libshmvar.so \
 		/usr/trimui/lib/libtmenu.so
 		do
+			BIND_PATH="$DST_DIR/$(basename "$f")"
 			# Ensure destination file exists
-			touch "$DST_DIR/$f"
+			touch "BIND_PATH"
 			# Bind mount the source file over it
-			mount --bind "$SRC_DIR/$f" "$DST_DIR/$f"
+			mount --bind "$f" "$BIND_PATH"
 		done
 		;;
 esac
