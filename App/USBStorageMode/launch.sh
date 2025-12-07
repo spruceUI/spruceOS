1a0afbef0a9de3233ef395364c7daa5ee909274c
Ryan Sartor <ryanmsartor@gmail.com>
2025-12-06 18:22:32 -0600

    condense usbstoragemode launch script a little

--- App/USBStorageMode/launch.sh
+++ App/USBStorageMode/launch.sh
@@ -6,8 +6,6 @@ HERE="$(dirname "$0")"
 cd "$HERE"
 
 case "$PLATFORM" in
-    "A30" ) ./launch_A30.sh ;;
-    "Brick" ) ./launch_Brick.sh ;;
     "SmartPro" ) exit 20 ;;
-    "Flip" ) ./launch_Flip.sh ;;
+    * ) ./launch_${PLATFORM}.sh ;;
 esac
