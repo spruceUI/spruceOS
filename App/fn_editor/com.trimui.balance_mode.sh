6d064ce5bc1843ea506ef4ec51bf98ec1238553d
Ryan Sartor <ryanmsartor@gmail.com>
2025-12-06 18:06:58 -0600

    lil more cleanup

--- App/fn_editor/com.trimui.balance_mode.sh
+++
@@ -1,3 +0,0 @@
-echo ondemand > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor                                                    
-echo -n "1200000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq 
-echo -n "2000000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq 
--- App/fn_editor/com.trimui.power_save_mode.sh
+++
@@ -1,3 +0,0 @@
-echo ondemand > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-echo -n "1200000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq 
-echo -n "1008000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq 
