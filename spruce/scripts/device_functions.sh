a4fe5732b1ffe43f4d3ca7eb6b536f49ea86556f
chrisj951 <chrisj951@gmail.com>
2025-12-15 14:29:37 -0500

    Move more platform specific code into the device_function.sh file

--- spruce/scripts/device_functions.sh
+++ spruce/scripts/device_functions.sh
@@ -598,3 +598,64 @@ set_path_variable() {
         export PATH="/mnt/SDCARD/spruce/bin64:$PATH"
     fi
 }
+
+
+enter_sleep() {
+    log_message "powerbutton_watchdog.sh: Entering sleep."
+    [ "$PLATFORM" = "Flip" ] && echo deep >/sys/power/mem_sleep
+    echo -n mem >/sys/power/state
+}
+
+get_current_volume() {
+    case "$PLATFORM" in
+        "Flip" ) amixer get 'SPK' | sed -n 's/.*Mono: *\([0-9]*\).*/\1/p' | tr -d '[]%' ;;
+        * ) amixer get 'Soft Volume Master' | sed -n 's/.*Front Left: *\([0-9]*\).*/\1/p' | tr -d '[]%' ;;
+    esac
+}
+
+set_volume() {
+    new_vol="${1:-0}" # default to mute if no value supplied
+    case "$PLATFORM" in
+        "Flip" ) amixer cset name='SPK Volume' "$new_vol" ;;
+        * ) amixer set 'Soft Volume Master' "$new_vol" ;;
+    esac
+}
+
+
+reset_playback_pack() {
+  #TODO I think this should be Flip only
+  if [ "$PLATFORM" = "Flip" ] || [ "$PLATFORM" = "Brick" ] || [ "$PLATFORM" = "A30" ] || [ "$PLATFORM" = "SmartPro" ] || [ "$PLATFORM" = "SmartProS" ]; then
+    log_message "*** audioFunctions.sh: reset playback path" -v
+
+    current_path=$(amixer cget name="Playback Path" | grep  -o ": values=[0-9]*" | grep -o [0-9]*)
+    system_json_volume=$(cat $SYSTEM_JSON | grep -o '"vol":\s*[0-9]*' | grep -o [0-9]*)
+    current_vol_name="SYSTEM_VOLUME_$system_json_volume"
+    amixer sset 'SPK' 1% > /dev/null
+    amixer cset name='Playback Path' 0 > /dev/null
+    amixer cset name='Playback Path' "$current_path" > /dev/null
+    amixer sset 'SPK' "$SYSTEM_VOLUME_${!current_vol_name}"% > /dev/null
+  fi 
+}
+
+set_playback_path() {
+  #TODO I think this should be Flip only
+  if [ "$PLATFORM" = "Flip" ] || [ "$PLATFORM" = "Brick" ] || [ "$PLATFORM" = "A30" ] || [ "$PLATFORM" = "SmartPro" ] || [ "$PLATFORM" = "SmartProS" ]; then
+    volume_lv=$(amixer cget name='SPK Volume' | grep  -o ": values=[0-9]*" | grep -o [0-9]*)
+    log_message "*** audioFunctions.sh: Volume level: $volume_lv" -v
+
+    jack_status=$(cat /sys/class/gpio/gpio150/value) # 0 connected, 1 disconnected
+    log_message "*** audioFunctions.sh: Jack status: $jack_status" -v
+
+    # 0 OFF, 2 SPK, 3 HP
+    playback_path=$([ $jack_status -eq 1 ] && echo 2 || echo 3)
+    [ "$volume_lv" = 0 ] && [ "$playback_path" = 2 ] && playback_path=0
+    log_message "*** audioFunctions.sh: Playback path: $playback_path" -v
+
+    current_path=$(amixer cget name="Playback Path" | grep  -o ": values=[0-9]*" | grep -o [0-9]*)
+
+    amixer cset name='Playback Path' "$playback_path" > /dev/null
+    # if coming off mute, ensure that there's a change so that volume doesn't spike
+    ( (( current_path == 0 )) || (( current_path != playback_path )) ) && [ ! "$playback_path" = 0 ] \
+      && amixer sset 'SPK' 1% > /dev/null && amixer sset 'SPK' "$volume_lv%" > /dev/null
+  fi
+}
