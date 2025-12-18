8428b46b7cf790cd00ffd2cc5d8f163457a4f083
chrisj951 <chrisj951@gmail.com>
2025-12-18 08:55:34 -0500

    Add volume watcher for smart pro s
    Also update filewatcher to allow forcing multiple rereads in
    m_time deadzones

--- App/PyUI/main-ui/devices/utils/file_watcher.py
+++ App/PyUI/main-ui/devices/utils/file_watcher.py
@@ -6,25 +6,31 @@ from utils.logger import PyUiLogger
 
 class FileWatcher():
 
-    def watch_file(self,path, callback, interval=1.0):
+    def watch_file(self,path, callback, interval, repeat_trigger_for_mtime_granularity_issues):
         last_mtime = None
+        prev_loop_executed_counter = 0.0
+        last_mtime = os.stat(path).st_mtime_ns
         while True:
             try:
-                mtime = os.path.getmtime(path)
-                if last_mtime is None:
-                    last_mtime = mtime
-                elif mtime != last_mtime:
+                mtime = os.stat(path).st_mtime_ns
+                if mtime != last_mtime:
+                    PyUiLogger.get_logger().info(f"{path} detection changed.")        
                     last_mtime = mtime
                     callback()
+                    if repeat_trigger_for_mtime_granularity_issues:
+                       prev_loop_executed_counter = 2.0
+                elif(prev_loop_executed_counter > 0.0):
+                    callback()
+                    prev_loop_executed_counter -= interval
             except FileNotFoundError:
                 PyUiLogger.get_logger().warning(f"{path} not found for file watcher.")        
             time.sleep(interval)
 
-    def start_file_watcher(self,path, callback, interval=1.0):
+    def start_file_watcher(self,path, callback, interval=1.0, repeat_trigger_for_mtime_granularity_issues=False):
         stop_event = threading.Event()
         thread = threading.Thread(
             target=self.watch_file,
-            args=(path, callback, interval),
+            args=(path, callback, interval, repeat_trigger_for_mtime_granularity_issues),
             daemon=True
         )
         thread.start()
