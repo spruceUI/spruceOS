135b6f0dff9c79044a23029605ddc6500a49ce7a
chrisj951 <chrisj951@gmail.com>
2026-01-06 17:36:04 -0500

    Lower flip powersave max freq to 816Mhz
    Works for PSX and below still
    
    gpSP works, but mGBA needs faster still

--- spruce/scripts/platform/device_functions/Flip.sh
+++ spruce/scripts/platform/device_functions/Flip.sh
@@ -263,7 +263,7 @@ set_powersave(){
     #408000 600000 816000 1104000 1416000 1608000 1800000 1992000
     echo "conservative" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
     echo "408000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-    echo "1104000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+    echo "816000" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
     echo "1" > /sys/devices/system/cpu/cpu0/online
     echo "0" > /sys/devices/system/cpu/cpu1/online
     echo "0" > /sys/devices/system/cpu/cpu2/online
