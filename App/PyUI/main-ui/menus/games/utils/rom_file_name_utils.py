503ebd9d6942e6258b4dc2ae4b6e532dfdbe732a
chrisj951 <chrisj951@gmail.com>
2026-01-16 07:45:27 -0500

    Fix collections (pico 8 might not show boxart for them though)

--- App/PyUI/main-ui/menus/games/utils/rom_file_name_utils.py
+++ App/PyUI/main-ui/menus/games/utils/rom_file_name_utils.py
@@ -7,6 +7,10 @@ class RomFileNameUtils:
     def get_rom_name_without_extensions(game_system, file_path) -> str:
         # Remove all known extensions from the filename
         base_name = os.path.splitext(os.path.basename(file_path))[0]
+        if(game_system is None):
+            # TODO will create issues for pico 8 boxart potentially
+            # But better than crashing for now
+            return base_name
         ext_list = game_system.game_system_config.get_extlist()
         while True:
             next_base, next_ext = os.path.splitext(base_name)
