cf0d6e47b59ce242a509e308319033f130313530
cobaltgit <cobaltsecuremail312@proton.me>
2026-01-17 21:35:59 +0000

    SmartProS: kill thermal-watchdog and turn off fan before going to sleep

--- spruce/scripts/platform/device_functions/SmartProS.sh
+++ spruce/scripts/platform/device_functions/SmartProS.sh
@@ -408,6 +408,7 @@ device_exit_sleep(){
         fi
     fi
     device_run_tsps_blobs
+    [ "$custom_thermal_watchdog" = "Custom" ] && /mnt/SDCARD/spruce/smartpros/bin/thermal-watchdog
 }
 
 WAKE_ALARM_PATH="/sys/class/rtc/rtc0/wakealarm"
@@ -428,7 +429,9 @@ device_enter_sleep() {
 
 
     save_sleep_info "$IDLE_TIMEOUT" || return 1
-    set_wake_alarm "$IDLE_TIMEOUT" "$WAKE_ALARM_PATH" || return 1
+    set_wake_alarm "$IDaLE_TIMEOUT" "$WAKE_ALARM_PATH" || return 1
+    pidof thermal-watchdog >/dev/null 2>&1 && killall thermal-watchdog
+    echo 0 > /sys/class/thermal/cooling_device0/cur_state
     trigger_device_sleep
 }
 
