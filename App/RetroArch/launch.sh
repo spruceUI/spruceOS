3ad699996a264127072f7c553c7a5533910512ff
Hairo R. Carela <hairocr8@gmail.com>
2026-01-23 21:20:31 -0400

    Fix retroarch not starting on the pixel2

--- App/RetroArch/launch.sh
+++ App/RetroArch/launch.sh
@@ -12,10 +12,10 @@ cd $RA_DIR/
 RA_PARAMS="-v"
 if [ "$PLATFORM" = "Pixel2" ]; then
     CURRENT_CFG=$(get_ra_cfg_location)
-    RA_PARAMS="${RA_PARAMS} --config $CURRENT_CFG"
+    RA_PARAMS="${RA_PARAMS} --config ${CURRENT_CFG}"
 fi
 
-HOME="$RA_DIR/" "$RA_DIR/$RA_BIN" "$RA_PARAMS"
+HOME="$RA_DIR/" "$RA_DIR/$RA_BIN" $RA_PARAMS
 
 backup_ra_config 2>/dev/null
 
