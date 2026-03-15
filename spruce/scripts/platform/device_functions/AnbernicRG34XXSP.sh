55995dccc062e51bdbf76f53eee319907240f31d
chrisj951 <chrisj951@gmail.com>
2026-03-14 20:20:47 -0400

    Checkpoint for getting 64bit ra to run on 34xxSP
    L2+R2 to exit

--- spruce/scripts/platform/device_functions/AnbernicRG34XXSP.sh
+++ spruce/scripts/platform/device_functions/AnbernicRG34XXSP.sh
@@ -52,7 +52,7 @@ setup_for_retroarch_and_get_bin_location(){
     #export CORE_DIR="/mnt/SDCARD/RetroArch/.retroarch/cores"
 
 
-    /mnt/SDCARD/RetroArch/.config/retroarch/autoconfig/sdl2
+    #/mnt/SDCARD/RetroArch/.config/retroarch/autoconfig/sdl2
 	RA_DIR="/mnt/SDCARD/RetroArch"
 	export RA_BIN="ra64.universal"
     export CORE_DIR="/mnt/SDCARD/RetroArch/.retroarch/cores64"
@@ -63,8 +63,9 @@ setup_for_retroarch_and_get_bin_location(){
 	else
 		export CORE_PATH="$CORE_DIR/${CORE}_libretro.so"
 	fi
-   
-   
+    
+    cp /mnt/SDCARD/RetroArch/platform/retroarch-AnbernicRG34XXSP-universal.cfg /mnt/SDCARD/RetroArch/.config/retroarch/retroarch.cfg
+    
     echo "$RA_BIN"
 }
 
@@ -127,7 +128,11 @@ runtime_mounts_anbernic_34xxsp() {
 device_init() {
     runtime_mounts_anbernic_34xxsp
 
-    /mnt/SDCARD/anbernic_adbd/run_adbd.sh &
+    {
+        sleep 10
+        /mnt/SDCARD/anbernic_adbd/run_adbd.sh &
+    } &
+    /mnt/SDCARD/spruce/rg34xxsp/bin/joypad_shim &
 }
 
 set_event_arg_for_idlemon() {
