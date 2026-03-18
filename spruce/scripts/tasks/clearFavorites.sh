e87564008c1b263070894bd0958337265f05354e
SundownerSport <kilchisblue@aol.com>
2026-03-18 15:10:22 -0700

    Add Clear all Recents and Clear all Favorites tasks
    Add task scripts to delete pyui-recents.json and pyui-favorites.json, and register them in spruce-tasks.json.

---
+++ spruce/scripts/tasks/clearFavorites.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+. /mnt/SDCARD/spruce/scripts/helperFunctions.sh
+
+rm -f /mnt/SDCARD/Saves/pyui-favorites.json
+
+log_message "Favorites: Cleared by request of user."
---
+++ spruce/scripts/tasks/clearRecents.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+. /mnt/SDCARD/spruce/scripts/helperFunctions.sh
+
+rm -f /mnt/SDCARD/Saves/pyui-recents.json
+
+log_message "Recents: Cleared by request of user."
