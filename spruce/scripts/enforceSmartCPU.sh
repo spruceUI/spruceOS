09c0dbba0dc9272a8177790159ac09f68be31ae7
Ryan Sartor <ryanmsartor@gmail.com>
2026-03-22 14:37:37 -0500

    delete vestigial read_button.sh; remove portion of enforceSmartCPU.sh that pauses and resumes getevent, since we're no longer wrestling for governor control with ra32.miyoo and cannot collide in that manner; move update_miyoo_payload.sh (currently unused) from spruce/scripts/ to spruce/flip/

--- spruce/scripts/enforceSmartCPU.sh
+++ spruce/scripts/enforceSmartCPU.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 . "/mnt/SDCARD/spruce/scripts/helperFunctions.sh"
-GOVERNOR_FILE="/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
+
 sleep 10
-# lock menu button to prevent ra32 menu from changing governor before we can lock out its write permission
-killall -19 getevent
 log_message "Enforcing SMART mode"
 set_smart "$1"
-# re-enable menu button now that ra32 can't reset the governor
-killall -18 getevent 
+
