10192871c985d7af504b243addc2c56ec68169ee
Ryan Sartor <ryanmsartor@gmail.com>
2026-03-25 17:49:37 -0500

    cleaner output from get-ra-version.sh: now programatically returns simply "1.22.2 (69a4f0e)"

--- App/PyUI/get-ra-version.sh
+++ App/PyUI/get-ra-version.sh
@@ -2,4 +2,5 @@
 
 . /mnt/SDCARD/spruce/scripts/helperFunctions.sh
 
-"/mnt/SDCARD/RetroArch/$RA_BIN" --version | grep "Version" 2>/dev/null
+"/mnt/SDCARD/RetroArch/$RA_BIN" --version 2>/dev/null \
+| sed -n 's/^Version: \([0-9.]*\) (Git \([a-f0-9]*\)).*/\1 (\2)/p'
