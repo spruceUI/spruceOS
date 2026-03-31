ed8884133a320e9461db4c781b8c7bea91083fe7
chrisj951 <chrisj951@gmail.com>
2026-03-30 20:40:13 -0400

    PyUI speed up optimizations

--- App/PyUI/main-ui/menus/games/utils/rom_info.py
+++ App/PyUI/main-ui/menus/games/utils/rom_info.py
@@ -9,16 +9,15 @@ from menus.games.utils.rom_file_name_utils import RomFileNameUtils
 
 @dataclass
 class RomInfo:
-    game_system: GameSystem
+    __slots__ = ("game_system", "rom_file_path", "is_collection", "display_name")
+    
+    game_system: "GameSystem"
     rom_file_path: str
     is_collection: bool
     display_name: str
 
-    def __init__(self, game_system: GameSystem, rom_file_path: str, display_name=None, is_collection=False):
+    def __init__(self, game_system: "GameSystem", rom_file_path: str, display_name=None, is_collection=False):
         self.game_system = game_system
         self.rom_file_path = rom_file_path
-        self.display_name = display_name
-        if(self.display_name is None):
-            self.display_name = RomFileNameUtils.get_rom_name_without_extensions(game_system,rom_file_path)
+        self.display_name = display_name or RomFileNameUtils.get_rom_name_without_extensions(game_system, rom_file_path)
         self.is_collection = is_collection
-        
