e09f4e8bed1adf688decf8cbcbcd51015d89b534
cobaltgit <cobaltsecuremail312@proton.me>
2026-07-11 14:15:00 +0100

    BrickPro: get RA off the ground first

--- spruce/scripts/emu/lib/ra_functions.sh
+++ spruce/scripts/emu/lib/ra_functions.sh
@@ -215,7 +215,7 @@ run_retroarch() {
 		RA_PARAMS="-v"
 	fi
 	case "$PLATFORM" in
-		"Pixel2"|"Flip"|"SmartPro"|"SmartProS"|"Brick"|"A30"|"MiyooMini"|"Anbernic"*)
+		"Pixel2"|"Flip"|"SmartPro"|"SmartProS"|"Brick"|"BrickPro"|"A30"|"MiyooMini"|"Anbernic"*)
 			RA_PARAMS="${RA_PARAMS} --config ${PLATFORM_CFG}"
 			;;
 	esac
