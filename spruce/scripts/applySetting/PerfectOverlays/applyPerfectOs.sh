08b2b16d9b104e25961102c9a11306d16b5539b0
Ryan Sartor <89538678+ryanmsartor@users.noreply.github.com>
2026-08-28 22:18:10 -0500

    Attempt to automate GBAOffset filter for Perfect Overlay setting:
    If on a Miyoo Mini, apply the filter if POs are on but filter is not found in gpSP.cfg. If not on Miyoo Mini, remove the filter if POs are on and filter is found in gpSP.cfg. If POs are turned off, remove the offset filter regardless of device.

--- spruce/scripts/applySetting/PerfectOverlays/applyPerfectOs.sh
+++ spruce/scripts/applySetting/PerfectOverlays/applyPerfectOs.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 
 . /mnt/SDCARD/spruce/scripts/helperFunctions.sh
+. /mnt/SDCARD/spruce/scripts/retroarch_utils.sh
+
+apply_offset_filter() {
+    log_message "Applying GBAOffset.filt for MiyooMini"
+    update_ra_config_file_with_new_setting \
+        /mnt/SDCARD/RetroArch/.retroarch/config/gpSP/gpSP.cfg \
+        "video_filter = \":/.retroarch/filters/video/GBAOffset.filt\""
+    update_ra_config_file_with_new_setting \
+        /mnt/SDCARD/RetroArch/.retroarch/config/mGBA/mGBA.cfg \
+        "video_filter = \":/.retroarch/filters/video/GBA/GBAOffset.filt\""
+}
+
+remove_offset_filter() {
+    log_message "Removing GBAOffset.filt"
+    update_ra_config_file_with_new_setting \
+        /mnt/SDCARD/RetroArch/.retroarch/config/gpSP/gpSP.cfg \
+        "video_filter = \"\""
+    update_ra_config_file_with_new_setting \
+        /mnt/SDCARD/RetroArch/.retroarch/config/mGBA/mGBA.cfg \
+        "video_filter = \"\""
+}
 
 if [ "$1" = "True" ]; then
     if ! flag_check "perfectOverlays"; then
@@ -10,8 +31,24 @@ if [ "$1" = "True" ]; then
         log_message "Turning on Perfect Overlays"
         flag_add "perfectOverlays"
     fi
+
+    case "$PLATFORM" in
+        "MiyooMini"*)
+            if ! grep -q "GBAOffset.filt" /mnt/SDCARD/RetroArch/.retroarch/config/gpSP/gpSP.cfg; then
+                apply_offset_filter
+            fi
+            ;;
+        *)
+            if grep -q "GBAOffset.filt" /mnt/SDCARD/RetroArch/.retroarch/config/gpSP/gpSP.cfg; then
+                remove_offset_filter
+            fi
+            ;;
+    esac
+
+
 elif [ "$1" = "False" ]; then
     if flag_check "perfectOverlays"; then
+        remove_offset_filter
         /mnt/SDCARD/spruce/scripts/applySetting/PerfectOverlays/GB.sh remove
         /mnt/SDCARD/spruce/scripts/applySetting/PerfectOverlays/GBC.sh remove
         /mnt/SDCARD/spruce/scripts/applySetting/PerfectOverlays/GBA.sh remove
