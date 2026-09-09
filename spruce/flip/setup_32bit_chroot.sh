a93c6e201b45771bf9027bfc29f9249c61d5c53a
SundownerSport <kilchisblue@aol.com>
2026-09-08 17:43:59 -0700

    flip chroot: drop the dead Roms/PORTS bind
    The chroot's SDCARD/ dir never existed, so the mount always failed. Nothing
    runs inside the chroot; it is only a lib32 source.

--- spruce/flip/setup_32bit_chroot.sh
+++ spruce/flip/setup_32bit_chroot.sh
@@ -11,6 +11,5 @@ mount --bind /proc /mnt/SDCARD/Persistent/.32bit_chroot/proc
 mount --bind /var/run /mnt/SDCARD/Persistent/.32bit_chroot/var/run
 mount --bind /mnt/sdcard /mnt/SDCARD/Persistent/.32bit_chroot/sdcard
 mount --bind /mnt/sdcard /mnt/SDCARD/Persistent/.32bit_chroot/mnt/sdcard
-mount --bind /mnt/SDCARD/Roms/ports/ /mnt/SDCARD/Persistent/.32bit_chroot/SDCARD/Roms/PORTS/
 
 chroot /mnt/SDCARD/Persistent/.32bit_chroot /bin/sh -c "/mnt/SDCARD/spruce/flip/mount_muOS.sh"
