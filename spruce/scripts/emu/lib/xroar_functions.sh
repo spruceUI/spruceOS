6e6661198c15b4ed7caa0c7aff087403271c591f
ImCoKeMaN <25371653+ImCoKeMaN@users.noreply.github.com>
2026-09-10 18:37:40 -0400

    Update run_xroar function to use ROM_FILE

--- spruce/scripts/emu/lib/xroar_functions.sh
+++ spruce/scripts/emu/lib/xroar_functions.sh
@@ -17,30 +17,18 @@ run_xroar() {
 
 XROAR_BIN="xroar"
 
-
-LD_LIBRARY_PATH="$XROAR_DIR/libs:$LD_LIBRARY_PATH"
+LD_LIBRARY_PATH="$EMU_DIR/libs:$LD_LIBRARY_PATH"
 export LD_LIBRARY_PATH
-
-HOME="$(GET_VAR "device" "board/home")"
-export HOME
-
-SETUP_SDL_ENVIRONMENT
-
-SET_VAR "system" "foreground_process" "$XROAR_BIN"
-
-case "$CORE" in
-	xroar | ext-xroar) CORE="coco2bus" ;;
-esac
 
 XR_GPTK="$EMU_DIR/gptk"
 
-GAME_BN=$(basename "${FILE%.*}")
+GAME_BN=$(basename "${ROM_FILE%.*}")
 GPTK_SP="${XR_GPTK}/${GAME_BN}.gptk"
 
 [ ! -f "$GPTK_SP" ] && GPTK_SP="${XR_GPTK}/$XROAR_BIN.gptk"
 
-/mnt/SDCARD/spruce/bin64/gptokeyb "$XROAR_BIN" -c "$GPTK_SP" &
+/mnt/SDCARD/Persistent/portmaster/PortMaster/gptokeyb -k "$XROAR_BIN" -c "$GPTK_SP" &
 
-"$EMU_DIR/$XROAR_BIN" -c "$EMU_DIR/$XROAR_BIN.conf" -default-machine coco2bus "$ROMFILE"
+"$EMU_DIR/$XROAR_BIN" -c "$EMU_DIR/$XROAR_BIN.conf" -default-machine coco2bus "$ROM_FILE"
 
 }
