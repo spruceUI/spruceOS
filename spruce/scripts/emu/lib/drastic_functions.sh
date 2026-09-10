e04eb6f63760bad9c0b7b6b3eca53f0e0bd30740
SundownerSport <kilchisblue@aol.com>
2026-09-09 19:34:29 -0700

    NDS: run DraStic-trngaje on the Brick Pro like the Brick

--- spruce/scripts/emu/lib/drastic_functions.sh
+++ spruce/scripts/emu/lib/drastic_functions.sh
@@ -195,12 +195,15 @@ run_drastic_MiyooMini() {
 
 ### BRICK PRO ###
 
-run_drastic_BrickPro(){ # todo: fix trngaje
+run_drastic_BrickPro(){
     if [ "$CORE" = "DraStic-original" ]; then
 		export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/lib64 
 		run_drastic64
 	elif [ "$CORE" = "DraStic-Steward" ]; then
 		run_drastic_steward_Brick
+	elif [ "$CORE" = "DraStic-trngaje" ]; then
+		export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/lib64
+		run_drastic_trngaje_a133p
 	else
 		display_core_unrecognized_for_platform_message
 	fi
